1a112a8469ca8014e903cef8b4a086d374503ff0
HeyZeer0 <heyzeer0@yahoo.com.br>
2017-12-27 14:45:57 -0200

    Prepared to enhanced configurations

--- src/main/java/com/wynndevs/ConfigValues.java
+++ src/main/java/com/wynndevs/ConfigValues.java
@@ -12,12 +12,16 @@ import java.util.UUID;
 @Config(modid = Reference.MOD_ID, name = Reference.MOD_ID)
 public class ConfigValues {
 
-    @GuiConfig(title = "Rich Presence", isInstance = true)
+    @GuiConfig(title = "RichPresence", isInstance = true)
     @Config.LangKey("config.richpresence")
     public static WynncraftRichPresence wynnRichPresence = new WynncraftRichPresence();
 
     @Config.LangKey("config.market")
     public static ConfigValues.Account marketAccount = new Account();
+
+    @GuiConfig(title = "Enhanced", isInstance = true)
+    @Config.LangKey("config.enhanced")
+    public static Enhanced wynnEnhanced = new Enhanced();
 
     @GuiConfig(title = "Receive chat mention notifications")
     @Config.Comment("Do you want to mention notifications ?")
@@ -31,6 +35,15 @@ public class ConfigValues {
 
         @Config.LangKey("config.richpresence.discord")
         public Discord discordConfig = new Discord();
+
+    }
+
+    public static class Enhanced {
+
+        //to EHTYCSCYTHE
+        //place your configs here, and if you want to create sub-interfaces feel free
+        //@GuiConfig(title = "the string that will show at the config") -- to boolean values
+        //@GuiConfig(title = "the string that wills how at the config", interface = true) -- to instance values like the wynnRichPresence method
 
     }
 
