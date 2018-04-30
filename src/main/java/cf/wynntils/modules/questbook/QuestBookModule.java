c9e49bd94b8523e27eed12894f5cbd8ee84e4dc0
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-04-30 14:56:39 -0300

    Oops forgot this

--- src/main/java/cf/wynntils/modules/questbook/QuestBookModule.java
+++ src/main/java/cf/wynntils/modules/questbook/QuestBookModule.java
@@ -6,6 +6,7 @@ package cf.wynntils.modules.questbook;
 
 import cf.wynntils.core.framework.instances.Module;
 import cf.wynntils.core.framework.interfaces.annotations.ModuleInfo;
+import cf.wynntils.modules.questbook.events.ClientEvents;
 import cf.wynntils.modules.questbook.events.ServerEvents;
 import cf.wynntils.modules.questbook.managers.QuestManager;
 import org.lwjgl.input.Keyboard;
@@ -15,6 +16,7 @@ public class QuestBookModule extends Module {
 
     public void onEnable() {
         registerEvents(new ServerEvents());
+        registerEvents(new ClientEvents());
 
         registerKeyBinding("requestUpdate", Keyboard.KEY_O, "tests", true, QuestManager::requestQuestBookReading);
     }
