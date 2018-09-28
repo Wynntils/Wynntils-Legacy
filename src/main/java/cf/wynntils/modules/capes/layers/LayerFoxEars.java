f70ebf28dc1a2268ae945ccb2130ef37dc41b8b6
DevScyu <SyaoranKawayama@gmail.com>
2018-09-28 19:49:23 +0100

    ocd
    Signed-off-by: DevScyu <SyaoranKawayama@gmail.com>

--- src/main/java/cf/wynntils/modules/capes/layers/LayerFoxEars.java
+++ src/main/java/cf/wynntils/modules/capes/layers/LayerFoxEars.java
@@ -25,7 +25,7 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
                 GlStateManager.pushMatrix();
                 GlStateManager.rotate(f, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(f1, 1.0F, 0.0F, 0.0F);
-                GlStateManager.translate(0.375F * (float) (i * 1.1 - 1), 0.0F, 0.0F);
+                GlStateManager.translate(0.375F * (float) (i * 1.2 - 1), 0.0F, 0.0F);
                 GlStateManager.translate(0.0F, -0.5F, 0.0F);
                 GlStateManager.rotate(f1, 0.5F, 0.0F, 0.0F);
                 GlStateManager.rotate(45f, 0.0F, 0.0F, 1.0F);
@@ -41,16 +41,16 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
     public void renderModel(AbstractClientPlayer player, ModelBase model, float scale) {
         this.bipedFoxEar = new ModelRenderer(model, 24, 0);
         if (player.isSneaking()) {
-            this.bipedFoxEar.addBox(2F, -1.5F, 2F, 3, 3, 1);
+            this.bipedFoxEar.addBox(0.0F + 4F, -5.0F + 5F / 2F, -1.0F, 6, 6, 1);
         } else {
-            this.bipedFoxEar.addBox(-0.1F, -3.0F, -1.0F, 3, 3, 1);
+            this.bipedFoxEar.addBox(0.0F, -5.0F, -1.0F, 6, 6, 1);
         }
-        ModelBase.copyModelAngles(this.playerRenderer.getMainModel().bipedHead, this.bipedFoxEar);
+        ModelBase.copyModelAngles(this.playerRenderer.getMainModel().bipedHeadwear, this.bipedFoxEar);
         this.bipedFoxEar.rotateAngleZ = 45.0f;
         this.bipedFoxEar.rotateAngleX = 0f;
         this.bipedFoxEar.rotateAngleY = 0f;
         this.bipedFoxEar.rotateAngleZ = 0f;
-        this.bipedFoxEar.render(scale);
+        this.bipedFoxEar.render(scale / 2);
     }
 
     public boolean shouldCombineTextures() {
