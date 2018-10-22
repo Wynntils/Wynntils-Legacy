1f70e0c18e9e6988c4f03a1a7f2e432e34b11344
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-10-22 16:51:04 -0300

    Another Test

--- src/main/java/cf/wynntils/core/CoreManager.java
+++ src/main/java/cf/wynntils/core/CoreManager.java
@@ -4,8 +4,6 @@ import cf.wynntils.core.events.ClientEvents;
 import net.minecraftforge.common.MinecraftForge;
 
 public class CoreManager {
-    
-    //Simple Test
 
     public static void setupCore() {
         MinecraftForge.EVENT_BUS.register(new ClientEvents());
