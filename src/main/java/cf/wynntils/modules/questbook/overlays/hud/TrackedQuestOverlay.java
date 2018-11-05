cca2f144a08371cde9d6f8179139fa5e4d01224b
Scyu_ <SyaoranKawayama@gmail.com>
2018-11-05 18:47:07 +0000

    Coords integration with quest
    Signed-off-by: Scyu_ <SyaoranKawayama@gmail.com>

--- src/main/java/cf/wynntils/modules/questbook/overlays/hud/TrackedQuestOverlay.java
+++ src/main/java/cf/wynntils/modules/questbook/overlays/hud/TrackedQuestOverlay.java
@@ -4,6 +4,7 @@ import cf.wynntils.core.framework.overlays.Overlay;
 import cf.wynntils.core.framework.rendering.SmartFontRenderer;
 import cf.wynntils.core.framework.rendering.colors.CommonColors;
 import cf.wynntils.modules.questbook.managers.QuestManager;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
 public class TrackedQuestOverlay extends Overlay {
@@ -26,6 +27,33 @@ public class TrackedQuestOverlay extends Overlay {
         for(String message : QuestManager.getTrackedQuest().getSplittedDescription()) {
             drawString(message, 0, 10+currentY, CommonColors.WHITE, SmartFontRenderer.TextAlignment.LEFT_RIGHT, SmartFontRenderer.TextShadow.OUTLINE);
             currentY+=10;
+        }
+
+        String desc = QuestManager.getTrackedQuest().getCurrentDescription();
+
+        String[] split = desc.split("\\[.{1,5}\\,\\d{1,5}\\,.{1,5}\\]");
+
+        if (split.length > 0) {
+            desc = desc.replace(split[0], "");
+            if (split.length > 1 && split[1] != null) desc = desc.replace(split[1], "");
+
+            String[] coords = desc.split(",");
+
+            if (coords.length == 3) {
+                int x = Integer.parseInt(coords[0].replace("[", ""));
+                int y = Integer.parseInt(coords[1]);
+                int z = Integer.parseInt(coords[2].replace("]", ""));
+
+                BlockPos pos = new BlockPos(x, y, z);
+                mc.world.setSpawnPoint(pos);
+            } else {
+                // 469 67 -1583
+                int x = 469;
+                int y = 67;
+                int z = -1583;
+                BlockPos pos = new BlockPos(x, y, z);
+                mc.world.setSpawnPoint(pos);
+            }
         }
     }
 
