eddf380053b0fd62f58a59c0898c256d62f57f49
HeyZeer0 <heyzeer0@yahoo.com.br>
2018-11-12 19:24:55 -0200

    Fixed a simple error

--- src/main/java/cf/wynntils/modules/core/events/ClientEvents.java
+++ src/main/java/cf/wynntils/modules/core/events/ClientEvents.java
@@ -46,7 +46,7 @@ public class ClientEvents implements Listener {
     @SubscribeEvent
     public void changeClass(GuiOverlapEvent.ChestOverlap.HandleMouseClick e) {
         if(e.getGuiInventory().getLowerInv().getName().contains("Select a Class")) {
-            if(e.getMouseButton() == 0 && e.getSlotIn().getHasStack() && e.getSlotIn().getStack().hasDisplayName() && e.getSlotIn().getStack().getDisplayName().contains("[>] Select")) {
+            if(e.getMouseButton() == 0 && e.getSlotIn() != null && e.getSlotIn().getHasStack() && e.getSlotIn().getStack().hasDisplayName() && e.getSlotIn().getStack().getDisplayName().contains("[>] Select")) {
                 PlayerInfo.getPlayerInfo().setClassId(e.getSlotId());
             }
         }
