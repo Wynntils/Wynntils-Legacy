c99579fff89e3f2d8c90a82ea1442bab7bcd3bd9
Scyu_ <SyaoranKawayama@gmail.com>
2018-11-17 22:25:01 +0000

    Fixed ears falling off your head.
    Signed-off-by: Scyu_ <SyaoranKawayama@gmail.com>

--- src/main/java/cf/wynntils/modules/capes/layers/LayerFoxEars.java
+++ src/main/java/cf/wynntils/modules/capes/layers/LayerFoxEars.java
@@ -20,16 +20,15 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
         if (WebManager.hasEars(entitylivingbaseIn.getUniqueID()) && entitylivingbaseIn.hasSkin() && !entitylivingbaseIn.isInvisible()) {
             this.playerRenderer.bindTexture(entitylivingbaseIn.getLocationSkin());
             for (int i = 0; i < 2; ++i) {
-                float f = entitylivingbaseIn.prevRotationYaw + (entitylivingbaseIn.rotationYaw - entitylivingbaseIn.prevRotationYaw) * partialTicks - (entitylivingbaseIn.prevRenderYawOffset + (entitylivingbaseIn.renderYawOffset - entitylivingbaseIn.prevRenderYawOffset) * partialTicks);
+                float f = entitylivingbaseIn.prevRotationYawHead + (entitylivingbaseIn.rotationYawHead - entitylivingbaseIn.prevRotationYawHead) * partialTicks - (entitylivingbaseIn.prevRenderYawOffset + (entitylivingbaseIn.renderYawOffset - entitylivingbaseIn.prevRenderYawOffset) * partialTicks);
                 float f1 = entitylivingbaseIn.prevRotationPitch + (entitylivingbaseIn.rotationPitch - entitylivingbaseIn.prevRotationPitch) * partialTicks;
                 GlStateManager.pushMatrix();
                 GlStateManager.rotate(f, 0.0F, 1.0F, 0.0F);
                 GlStateManager.rotate(f1, 1.0F, 0.0F, 0.0F);
                 GlStateManager.translate(0.375F * (float) (i * 1.2 - 1), 0.0F, 0.0F);
                 GlStateManager.translate(0.0F, -0.5F, 0.0F);
-                GlStateManager.rotate(f1, 0.5F, 0.0F, 0.0F);
                 GlStateManager.rotate(45f, 0.0F, 0.0F, 1.0F);
-
+//
                 GlStateManager.scale(1.3333334F, 1.3333334F, 1.3333334F);
                 renderModel(entitylivingbaseIn, this.playerRenderer.getMainModel(), 0.0625f);
                 GlStateManager.popMatrix();
@@ -40,15 +39,14 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
     public void renderModel(AbstractClientPlayer player, ModelBase model, float scale) {
         this.bipedFoxEar = new ModelRenderer(model, 24, 0);
         if (player.isSneaking()) {
-            this.bipedFoxEar.addBox(0.0F + 4F, -5.0F + 5F / 2F, -1.0F, 6, 6, 1);
+            this.bipedFoxEar.addBox(0.0F + 4F, -5.0F + 5F / 2F, 1f, 6, 6, 1);
         } else {
             this.bipedFoxEar.addBox(0.0F, -5.0F, -1.0F, 6, 6, 1);
         }
         ModelBase.copyModelAngles(this.playerRenderer.getMainModel().bipedHeadwear, this.bipedFoxEar);
-        this.bipedFoxEar.rotateAngleZ = 45.0f;
-        this.bipedFoxEar.rotateAngleX = 0f;
-        this.bipedFoxEar.rotateAngleY = 0f;
-        this.bipedFoxEar.rotateAngleZ = 0f;
+        this.bipedFoxEar.rotateAngleX = this.playerRenderer.getMainModel().bipedHeadwear.rotateAngleX * (scale / 2);
+        this.bipedFoxEar.rotateAngleY = this.playerRenderer.getMainModel().bipedHeadwear.rotateAngleY * (scale / 2);
+        this.bipedFoxEar.rotateAngleZ = this.playerRenderer.getMainModel().bipedHeadwear.rotateAngleZ * (scale / 2);
         this.bipedFoxEar.render(scale / 2);
     }
 
