6ae8019ede797b7112022e8110405c4c54e8065d
Nicholas <14227259+CringeAura@users.noreply.github.com>
2018-12-28 03:11:58 -0800

    Fixed grammar issues (#30)
    * Fix grammar issues

--- src/main/java/cf/wynntils/modules/questbook/configs/QuestBookConfig.java
+++ src/main/java/cf/wynntils/modules/questbook/configs/QuestBookConfig.java
@@ -8,15 +8,15 @@ import cf.wynntils.core.framework.settings.annotations.Setting;
 import cf.wynntils.core.framework.settings.annotations.SettingsInfo;
 import cf.wynntils.core.framework.settings.instances.SettingsClass;
 
-@SettingsInfo(name = "questbook", displayPath = "QuestBook")
+@SettingsInfo(name = "questbook", displayPath = "Quest Book")
 public class QuestBookConfig extends SettingsClass {
 
     public static QuestBookConfig INSTANCE;
 
-    @Setting(displayName = "Allow Wynntils Questbook", description = "Should the default questbook be replaced by our own")
+    @Setting(displayName = "Allow Wynntils' Questbook", description = "Should Wynncraft's quest book be replaced by Wynntils'?")
     public boolean allowCustomQuestbook = true;
 
-    @Setting(displayName = "Set quest location to compass", description = "Should the compass follow the quest coordinates")
+    @Setting(displayName = "Set Quest Location to Compass", description = "Should the compass point towards given coordinates of quests?")
     public boolean compassFollowQuests = true;
 
 }
