94385cad506fd7387b7074d9ef0be13adabb847b
Nicholas <14227259+CringeAura@users.noreply.github.com>
2018-12-27 22:20:06 -0800

    Update UtilitiesConfig.java

--- src/main/java/cf/wynntils/modules/utilities/configs/UtilitiesConfig.java
+++ src/main/java/cf/wynntils/modules/utilities/configs/UtilitiesConfig.java
@@ -79,7 +79,7 @@ public class UtilitiesConfig extends SettingsClass {
         @Setting(displayName = "Highlight Hotbar Items", description = "Should the items in your hotbar be highlighted?")
         public boolean hotbarHighlight = true;
 
-        @Setting(displayName = "Highlight Armor Items", description = "Should your worn armor be highlighted?")
+        @Setting(displayName = "Highlight Armor Items", description = "Should your worn armor be highlighted according to rarity?")
         public boolean armorHighlight = true;
 
         @Setting(displayName = "Highlight Mythics", description = "Should mythic items be highlighted?")
