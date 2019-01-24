65027983e7c784d8c30908ceb15eb6680bbf4441
HighCrit <dannydortmont@gmail.com>
2019-01-25 00:55:16 +0100

    This aint German
    One does not capitalize all the letters. This ain't German.

--- src/main/java/cf/wynntils/modules/map/configs/MapConfig.java
+++ src/main/java/cf/wynntils/modules/map/configs/MapConfig.java
@@ -11,14 +11,14 @@ public class MapConfig extends SettingsClass {
     @Setting(displayName = "Map Format", description = "Should the Map be a Square or a Circle?")
     public MapFormat mapFormat = MapFormat.CIRCLE;
 
-    @Setting(displayName = "Follow Player Rotation", description = "Should the Map Follow the Player's Rotation?")
+    @Setting(displayName = "Follow Player Rotation", description = "Should the Map follow the Player's Rotation?")
     public boolean followPlayerRotation = true;
 
-    @Setting(displayName = "Map Size", description = "How Large Should the Map be?")
+    @Setting(displayName = "Map Size", description = "How large should the Map be?")
     @Setting.Limitations.IntLimit(min = 100, max = 200, precision = 1)
     public int mapSize = 100;
 
-    @Setting(displayName = "Texture Style", description = "What Should the Map Texture be?")
+    @Setting(displayName = "Texture Style", description = "What should the Map Texture be?")
     public TextureType textureType = TextureType.Paper;
 
     public enum MapFormat {
