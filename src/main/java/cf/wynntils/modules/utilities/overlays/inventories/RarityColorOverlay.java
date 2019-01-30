b1ee7a1b05d904c717dc6f280b1f71cfbb34e3df
Bedo9041 <Bedo9041@users.noreply.github.com>
2019-01-30 17:10:15 +0000

    Highlight duplicate cosmetics on scrap menu

--- src/main/java/cf/wynntils/modules/utilities/overlays/inventories/RarityColorOverlay.java
+++ src/main/java/cf/wynntils/modules/utilities/overlays/inventories/RarityColorOverlay.java
@@ -198,6 +198,8 @@ public class RarityColorOverlay implements Listener {
                 r = 0; g = 1; b = 0;
             } else if (lore.contains("§fNormal") && UtilitiesConfig.Items.INSTANCE.normalHighlight) {
                 r = 1; g = 1; b = 1;
+            } else if (UtilitiesConfig.Items.INSTANCE.highlightCosmeticDuplicates && guiContainer.getSlotUnderMouse() != null && lore.contains("Reward") && guiContainer.getSlotUnderMouse().slotNumber != s.slotNumber && guiContainer.getSlotUnderMouse().getStack().getDisplayName().equals(is.getDisplayName())) {
+                r = 0f; g = 1f; b = 0f;
             } else if (lore.contains("§6Epic") && lore.contains("Reward") && UtilitiesConfig.Items.INSTANCE.epicEffectsHighlight) {
                 r = 1; g = 0.666f; b = 0;
             } else if (lore.contains("§cGodly") && lore.contains("Reward") && UtilitiesConfig.Items.INSTANCE.godlyEffectsHighlight) {
