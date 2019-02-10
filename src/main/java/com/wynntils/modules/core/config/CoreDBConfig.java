c1ebc5167872230b73712ec76fbf462e086c046b
Nicholas <14227259+CringeAura@users.noreply.github.com>
2019-02-10 12:36:33 -0800

    Update CoreDBConfig.java

--- src/main/java/com/wynntils/modules/core/config/CoreDBConfig.java
+++ src/main/java/com/wynntils/modules/core/config/CoreDBConfig.java
@@ -20,8 +20,8 @@ public class CoreDBConfig extends SettingsClass {
     public int lastSelectedClass = -1;
     public ClassType lastClass = ClassType.NONE;
 
-    @Setting(displayName = "Update Stream", description = "Which update stream should the mod be on?\n" +
-            "§2Stable: The mod will only update when a new version is released. Stable versions are generally more stable than Cutting Edge builds.\n" +
+    @Setting(displayName = "Update Stream", description = "Which update stream should the mod be on?\n\n" +
+            "§2Stable: The mod will only update when a new version is released. Stable versions are generally more stable than Cutting Edge builds.\n\n" +
             "§4Cutting Edge: The mod will update whenever a new build is release. Cutting Edge builds will include features that are currently in development, but may also be less stable than Stable versions.")
     public UpdateStream updateStream = UpdateStream.STABLE;
 
