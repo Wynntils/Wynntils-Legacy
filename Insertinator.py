cd30faf411319f0a54ee0555edcda37efe721f9f
HighCrit <35771251+HighCrit@users.noreply.github.com>
2019-04-08 00:50:29 +0200

    Added script to add new translation keys

---
+++ Insertinator.py
@@ -0,0 +1,36 @@
+import sys, os
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + "/src/main/resources/assets/wynntils/lang/"
+f = open(path + "en_us.lang", "r")
+en_us_data = f.readlines()
+f.close()
+
+for filename in os.listdir(path):
+    if (filename.endswith(".lang")):
+        if (filename.endswith("en_us.lang")):
+            continue
+
+        f = open(os.path.dirname(path) + "/" + filename, "r")
+        other_lang_data = f.readlines()
+        f.close()
+        inserts = 0
+        appends = 0
+        for i in range(len(en_us_data)):
+            try:
+                if (en_us_data[i].split("=")[0] != other_lang_data[i].split("=")[0] and (not en_us_data[i].startswith("#"))):
+                    inserts += 1
+                    #print("Inserted at {0}".format(i))
+                    if (len(en_us_data[i].strip()) == 0 or ("#" in en_us_data[i])):
+                        other_lang_data.insert(i, en_us_data[i])
+                    else:
+                        other_lang_data.insert(i, "{0} ##TODO\n".format(en_us_data[i].strip()))
+            except IndexError:
+                appends += 1
+                #print("Appended at {0}".format(i))
+                if (len(en_us_data[i].strip()) == 0 or ("#" in en_us_data[i])):
+                    other_lang_data.append(en_us_data[i])
+                else:
+                    other_lang_data.append("{0} ##TODO\n".format(en_us_data[i].strip()))
+        f = open(os.path.dirname(path) + "/" + filename, "w")
+        f.writelines(other_lang_data)
+        f.close()
+        print("Finished {0}, Inserted {1} line(s) and Appended {2} line(s)".format(filename, inserts, appends))
