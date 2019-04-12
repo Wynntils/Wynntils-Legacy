ccd09b5798f69824a829c40cf430930d1156d79f
Nicholas <14227259+CringeAura@users.noreply.github.com>
2019-04-12 16:55:44 -0700

    Update ChatConfig.java

--- src/main/java/com/wynntils/modules/chat/configs/ChatConfig.java
+++ src/main/java/com/wynntils/modules/chat/configs/ChatConfig.java
@@ -33,10 +33,10 @@ public class ChatConfig extends SettingsClass {
     @Setting(displayName = "Chat Spam Filter", description = "Should repeating messages stack rather than flood the chat?")
     public boolean blockChatSpamFilter = true;
 
-    @Setting(displayName = "Filter Info Messages", description = "Should Wynncraft Info messages be filtered? (Messages starting with §4[Info]§f will no longer appear in chat.)")
+    @Setting(displayName = "Filter Info Messages", description = "Should Wynncraft Info messages be filtered? (Messages starting with §4[Info]§r will no longer appear in chat.)")
     public boolean filterWynncraftInfo = true;
 
-    @Setting(displayName = "Filter Territory Enter", description = "Should territory enter messages be displayed in chat? (e.g [You are now entering Detlas]")
+    @Setting(displayName = "Filter Territory Enter", description = "Should territory enter messages be displayed in chat? (e.g. §7[You are now entering Detlas]§r)")
     public boolean filterTerritoryEnter = true;
 
     public boolean registeredDefaultTabs = false;
