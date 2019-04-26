534f370aa7973d3dc342e1db2085bfee7c55e15c
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-04-26 17:49:30 -0300

    Added highlight colors to Hotbar

--- src/main/java/com/wynntils/modules/utilities/overlays/hud/HotbarOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/hud/HotbarOverlay.java
@@ -6,11 +6,16 @@ package com.wynntils.modules.utilities.overlays.hud;
 
 import com.wynntils.ModCore;
 import com.wynntils.core.framework.overlays.Overlay;
-import com.wynntils.core.framework.rendering.ScreenRenderer;
+import com.wynntils.core.framework.rendering.colors.CustomColor;
 import com.wynntils.core.framework.rendering.textures.AssetsTexture;
 import com.wynntils.core.framework.rendering.textures.Textures;
+import com.wynntils.core.utils.Utils;
 import com.wynntils.modules.utilities.configs.OverlayConfig;
+import com.wynntils.modules.utilities.configs.UtilitiesConfig;
+import com.wynntils.webapi.WebManager;
+import com.wynntils.webapi.profiles.item.ItemProfile;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
@@ -22,16 +27,20 @@ public class HotbarOverlay extends Overlay {
         super("Hotbar", 182, 22, true, 0.5f, 1f, 0, -23, OverlayGrowFrom.TOP_CENTRE, RenderGameOverlayEvent.ElementType.HOTBAR);
     }
 
+
+
     @Override
     public void render(RenderGameOverlayEvent.Pre event) {
-        if (!WIDGETS_TEXTURE.loaded)
-            WIDGETS_TEXTURE.load();
+        if (!WIDGETS_TEXTURE.loaded) WIDGETS_TEXTURE.load();
+
         EntityPlayerSP player = ModCore.mc().player;
         int textureY = 0;
+
         switch (OverlayConfig.Hotbar.INSTANCE.hotbarTexture) {
             case Wynn: textureY = 0;
                 break;
         }
+
         if (OverlayConfig.Hotbar.INSTANCE.hotbarTexture == OverlayConfig.Hotbar.HotbarTextures.Resource_Pack) {
             float scale = WIDGETS_TEXTURE.height / 256;
             drawRect(WIDGETS_TEXTURE, -91, 0, 91, 22, 0, 0, (int) (182 * scale), (int) (22 * scale));
@@ -40,9 +49,28 @@ public class HotbarOverlay extends Overlay {
             drawRect(Textures.Overlays.hotbar, -91, 0, 0, textureY, 182, 22);
             drawRect(Textures.Overlays.hotbar, -92 + player.inventory.currentItem * 20, -1, 0, textureY + 22, 24, 22);
         }
-        ScreenRenderer renderer = new ScreenRenderer();
+
         for (int i = 0; i < 9; i++) {
-            renderer.drawItemStack(player.inventory.mainInventory.get(i), -88 + i * 20, 3);
+            ItemStack stack = player.inventory.mainInventory.get(i);
+
+            int x = -88 + (i*20);
+            if(!stack.isEmpty() && stack.hasDisplayName() && WebManager.getItems().containsKey(Utils.stripColor(stack.getDisplayName()))) {
+                ItemProfile wynnItem = WebManager.getItems().get(Utils.stripColor(stack.getDisplayName()));
+                CustomColor color;
+                switch (wynnItem.getTier()) {
+                    case MYTHIC: color = UtilitiesConfig.Items.INSTANCE.mythicHighlightColor; break;
+                    case LEGENDARY: color = UtilitiesConfig.Items.INSTANCE.lengendaryHighlightColor; break;
+                    case RARE: color = UtilitiesConfig.Items.INSTANCE.rareHighlightColor; break;
+                    case UNIQUE: color = UtilitiesConfig.Items.INSTANCE.uniqueHighlightColor; break;
+                    case SET: color = UtilitiesConfig.Items.INSTANCE.setHighlightColor; break;
+                    default: return;
+                }
+
+                color.setA(0.3f);
+                drawRect(color, x, 3, x + 16, 19);
+            }
+            drawItemStack(stack, x, 3);
         }
+
     }
 }
