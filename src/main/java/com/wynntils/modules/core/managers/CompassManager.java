848f072c524f206ae41701a42c19ab9e604d406c
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-06-08 11:43:26 -0300

    Fixed a simple crash

--- src/main/java/com/wynntils/modules/core/managers/CompassManager.java
+++ src/main/java/com/wynntils/modules/core/managers/CompassManager.java
@@ -13,7 +13,7 @@ public class CompassManager {
     private static Location compassLocation = null;
 
     public static Location getCompassLocation() {
-        compassLocation.setY(Minecraft.getMinecraft().player.posY);
+        if(compassLocation != null) compassLocation.setY(Minecraft.getMinecraft().player.posY);
         return compassLocation;
     }
 
