f2c94487aba203a2a542ce1b3767a524b0c01c75
coehlrich <coehlrich@users.noreply.github.com>
2019-10-14 16:03:00 +1300

    Add confirmation for purchasing bank pages that opens a yes/no GUI
    Signed-off-by: coehlrich <coehlrich@users.noreply.github.com>

--- src/main/java/com/wynntils/modules/utilities/configs/UtilitiesConfig.java
+++ src/main/java/com/wynntils/modules/utilities/configs/UtilitiesConfig.java
@@ -56,6 +56,9 @@ public class UtilitiesConfig extends SettingsClass {
     @Setting(displayName = "Apply Wynncraft Resource Pack", description = "Should the Wynncraft server resource pack be applied when joining the server instead of when picking your class?")
     public boolean autoResource = true;
 
+    @Setting(displayName = "Add Confirmation For Purchasing Bank Pages", description = "Should Wynntils show a yes no gui when buying bank pages?")
+    public boolean addBankConfirmation = true;
+
     @Setting(upload = false)
     public String lastServerResourcePack = "";
 
