5c7bbb555af6ed10336592d7581e3a5d0014f16b
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-11-22 19:24:25 -0300

    Forgot to invert this condition
    Signed-off-by: HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/modules/utilities/overlays/inventories/MenuButtonsOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/inventories/MenuButtonsOverlay.java
@@ -18,22 +18,22 @@ public class MenuButtonsOverlay implements Listener {
 
     @SubscribeEvent
     public void initGui(GuiOverlapEvent.IngameMenuOverlap.InitGui e) {
-        if (UtilitiesConfig.INSTANCE.addClassServer) {
-            ArrayList<GuiButton> toBeRemoved = new ArrayList<>();
-            e.getButtonList().forEach(b -> {
-                if (b.id >= 5 && b.id <= 7) {
-                    toBeRemoved.add(b);
-                } else if (b.id == 1) {
-                    b.displayString = TextFormatting.RED + b.displayString;
-                } else if (b.id == 12 || b.id == 0) {
-                    b.displayString = TextFormatting.GRAY + b.displayString;
-                }
-            });
+        if(!UtilitiesConfig.INSTANCE.addClassServer) return;
+        
+        ArrayList<GuiButton> toBeRemoved = new ArrayList<>();
+        e.getButtonList().forEach(b -> {
+            if (b.id >= 5 && b.id <= 7) {
+                toBeRemoved.add(b);
+            } else if (b.id == 1) {
+                b.displayString = TextFormatting.RED + b.displayString;
+            } else if (b.id == 12 || b.id == 0) {
+                b.displayString = TextFormatting.GRAY + b.displayString;
+            }
+        });
 
-            e.getButtonList().removeAll(toBeRemoved);
-            e.getButtonList().add(new GuiButton(753, e.getGui().width / 2 - 100, e.getGui().height / 4 + 48 + -16, "Class selection"));
-            e.getButtonList().add(new GuiButton(754, e.getGui().width / 2 - 100, e.getGui().height / 4 + 72 + -16, "Back to Hub"));
-        }
+        e.getButtonList().removeAll(toBeRemoved);
+        e.getButtonList().add(new GuiButton(753, e.getGui().width / 2 - 100, e.getGui().height / 4 + 48 + -16, "Class selection"));
+        e.getButtonList().add(new GuiButton(754, e.getGui().width / 2 - 100, e.getGui().height / 4 + 72 + -16, "Back to Hub"));
     }
 
     @SubscribeEvent
