a1290d1123c11e96a736969c88d20e108a85f6cc
Scyu_ <Scyu_@wynntils.com>
2019-11-28 23:44:03 +0000

    Additional Check
    Signed-off-by: Scyu_ <Scyu_@wynntils.com>

--- src/main/java/com/wynntils/modules/core/instances/PlayerLocationProfile.java
+++ src/main/java/com/wynntils/modules/core/instances/PlayerLocationProfile.java
@@ -59,15 +59,14 @@ public class PlayerLocationProfile {
      */
     public boolean updateFromWorld() {
         EntityPlayer e = entityRef.get();
-        boolean canRender = e != null && e.getDistance(Minecraft.getMinecraft().player) < 30;
-        if (e != null && canRender) {
+        if (e != null && e.getDistance(Minecraft.getMinecraft().player) < 30) {
             x = (int) e.posX;
             y = (int) e.posY;
             z = (int) e.posZ;
             return true;
         }
         e = PlayerEntityManager.getPlayerByUUID(uuid);
-        if (e == null) {
+        if (e == null || e.getDistance(Minecraft.getMinecraft().player) > 30) {
             return false;
         }
         hasHat = e.isWearing(EnumPlayerModelParts.HAT);
