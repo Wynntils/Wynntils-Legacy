db61f2309f8b688b268657a30c7b9ed89a2e3c36
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-12-02 00:33:34 -0300

    Cleaned some useless stuff
    Signed-off-by: HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/modules/utilities/managers/SpeedometerManager.java
+++ src/main/java/com/wynntils/modules/utilities/managers/SpeedometerManager.java
@@ -4,15 +4,11 @@
 
 package com.wynntils.modules.utilities.managers;
 
-import com.wynntils.core.utils.Location;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.util.math.MathHelper;
 
 public class SpeedometerManager {
-
-    private static long nextUpdate = 0;
-    private static Location lastLocation = null;
 
     public static double getCurrentSpeed() {
         EntityPlayerSP player = Minecraft.getMinecraft().player;
