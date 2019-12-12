dc8d7c528048d28f15db9ad59211e49029bcc252
Nicholas <14227259+CringeAura@users.noreply.github.com>
2019-12-11 17:01:42 -0800

    Update CoreDBConfig.java

--- src/main/java/com/wynntils/modules/core/config/CoreDBConfig.java
+++ src/main/java/com/wynntils/modules/core/config/CoreDBConfig.java
@@ -28,10 +28,10 @@ public class CoreDBConfig extends SettingsClass {
 
     @Setting(displayName = "Update Stream", description = "Which update stream should the mod be on?\n\n" +
             "§2Stable: §rThe mod will only update when a new version is released. Stable versions are generally more stable than Cutting Edge builds.\n\n" +
-            "§4Cutting Edge: §rThe mod will update whenever a new build is release. Cutting Edge builds will include features that are not yet in Stable versions and are currently in development but may also be less stable than Stable versions.", upload = false)
+            "§4Cutting Edge: §rThe mod will update whenever a new build is released. Cutting Edge builds will include features that are not yet in Stable versions and are currently in development but may also be less stable than Stable versions.", upload = false)
     public UpdateStream updateStream = UpdateStream.STABLE;
 
-    @Setting(displayName = "Enable Socket Connections", description = "Socket connections are used to:\n  - §7Display friend on the world map.§r\n  - §7Display party members on the world map.§r\n  - §7Display guild members on the world map.§r")
+    @Setting(displayName = "Enable Socket Connections", description = "Socket connections are used to:\n  - §7Display friends on the world map.§r\n  - §7Display party members on the world map.§r\n  - §7Display guild members on the world map.§r")
     public boolean enableSockets = true;
 
     @Setting(displayName = "Scroll Direction", description = "Which direction should your mouse scroll for the page to scroll down?")
