0e920f8739c684b7420df7fcc3ecf453cec87727
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2019-12-12 17:19:41 -0300

    Added reroll formulas to item tier
    Signed-off-by: HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/webapi/profiles/item/enums/ItemTier.java
+++ src/main/java/com/wynntils/webapi/profiles/item/enums/ItemTier.java
@@ -7,20 +7,22 @@ package com.wynntils.webapi.profiles.item.enums;
 import com.wynntils.core.utils.Utils;
 import net.minecraft.util.text.TextFormatting;
 
+import java.util.function.Function;
+
 public enum ItemTier {
 
-    NORMAL(0, TextFormatting.WHITE),
-    SET(1, TextFormatting.GREEN),
-    UNIQUE(2, TextFormatting.YELLOW),
-    RARE(3, TextFormatting.LIGHT_PURPLE),
-    LEGENDARY(4, TextFormatting.AQUA),
-    FABLED(5, TextFormatting.RED),
-    MYTHIC(6, TextFormatting.DARK_PURPLE);
+    NORMAL   (0, TextFormatting.WHITE,        (lvl) -> 0),
+    SET      (1, TextFormatting.GREEN,        (lvl) -> (int)Math.ceil(12d + lvl * 1.6)),
+    UNIQUE   (2, TextFormatting.YELLOW,       (lvl) -> (int)Math.ceil(5d + lvl * 0.5)),
+    RARE     (3, TextFormatting.LIGHT_PURPLE, (lvl) -> (int)Math.ceil(15d + lvl * 1.2)),
+    LEGENDARY(4, TextFormatting.AQUA,         (lvl) -> (int)Math.ceil(40d + lvl * 5.2)),
+    FABLED   (5, TextFormatting.RED,          (lvl) -> (lvl + 5) * 60),
+    MYTHIC   (6, TextFormatting.DARK_PURPLE,  (lvl) -> (int)Math.ceil(90d + lvl * 18));
 
-    int priority; String color;
+    int priority; String color; Function<Integer, Integer> rerollFormula;
 
-    ItemTier(int priority, TextFormatting color) {
-        this.priority = priority; this.color = color.toString();
+    ItemTier(int priority, TextFormatting color, Function<Integer, Integer> rerollFormula) {
+        this.priority = priority; this.color = color.toString(); this.rerollFormula = rerollFormula;
     }
 
     public int getPriority() {
@@ -29,6 +31,12 @@ public enum ItemTier {
 
     public String getColor() {
         return color;
+    }
+
+    public int getRerollPrice(int level, int rerollAmount) {
+        int basePrice = rerollFormula.apply(level);
+
+        return rerollAmount == 0 ? basePrice : basePrice * (int)Math.pow(5, rerollAmount);
     }
 
     public String asLore() {
