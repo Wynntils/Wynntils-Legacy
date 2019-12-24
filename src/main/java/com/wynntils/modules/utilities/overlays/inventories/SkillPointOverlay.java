27093c038037cdbdf333a776604842932f8f797a
Fabio Augusto <21368488+HeyZeer0@users.noreply.github.com>
2019-12-24 12:03:14 -0300

    Made SP Overlay analyse everything at once

--- src/main/java/com/wynntils/modules/utilities/overlays/inventories/SkillPointOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/inventories/SkillPointOverlay.java
@@ -7,6 +7,7 @@ package com.wynntils.modules.utilities.overlays.inventories;
 import com.wynntils.core.events.custom.GuiOverlapEvent;
 import com.wynntils.core.framework.interfaces.Listener;
 import com.wynntils.core.utils.ItemUtils;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class SkillPointOverlay implements Listener {
@@ -14,49 +15,56 @@ public class SkillPointOverlay implements Listener {
     private static final String colourPattern = "(?i)§[0-9A-FK-OR]";
 
     @SubscribeEvent
-    public void onChestInventory(GuiOverlapEvent.ChestOverlap.DrawGuiContainerForegroundLayer e) {
-        if (e.getGui().getSlotUnderMouse() == null || !e.getGui().getSlotUnderMouse().getHasStack()) return;
-
-        String lore = ItemUtils.getStringLore(e.getGui().getSlotUnderMouse().getStack());
-        String name = e.getGui().getSlotUnderMouse().getStack().getDisplayName();
+    public void onChestInventory(GuiOverlapEvent.ChestOverlap.DrawScreen e) {
         if (!e.getGui().getLowerInv().getName().contains("skill points remaining")) return;
-        
-        String value;
-        if (name.contains("Upgrade")) {// Skill Points
 
-            lore = lore.replaceAll(colourPattern, "");
-            String[] tokens = lore.split("[0-9]{1,3} points");
-            for (String token : tokens) {
-                lore = lore.replace(token, "");
+        for(int i = 0; i < e.getGui().getLowerInv().getSizeInventory(); i++) {
+            ItemStack stack = e.getGui().getLowerInv().getStackInSlot(i);
+            if(stack.isEmpty() || !stack.hasDisplayName() || stack.getTagCompound().hasKey("wynntilsAnalyzed")) continue; //display name also checks for tag compound
+
+            stack.getTagCompound().setBoolean("wynntilsAnalyzed", true);
+            String lore = ItemUtils.getStringLore(stack);
+            String name = stack.getDisplayName();
+
+            String value;
+            if (name.contains("Upgrade")) {// Skill Points
+
+                lore = lore.replaceAll(colourPattern, "");
+                String[] tokens = lore.split("[0-9]{1,3} points");
+                for (String token : tokens) {
+                    lore = lore.replace(token, "");
+                }
+                String[] numbers = lore.split(" ");
+                value = numbers[0];
+
+                System.out.println(value);
+            } else if (name.contains("Profession")) { // Profession Icons
+                lore = lore.replaceAll(colourPattern, "");
+
+                String[] tokens = lore.split("Level: [0-9]{1,3}");
+                for (String token : tokens) {
+                    lore = lore.replace(token, "");
+                }
+                String[] numbers = lore.split(" ");
+                value = numbers[1];
+            } else if (name.contains("'s Info")) { // Combat level on Info
+                lore = lore.replaceAll(colourPattern, "");
+
+                String[] tokens = lore.split("Combat Lv: [0-9]{1,3}");
+                for (String token : tokens) {
+                    lore = lore.replace(token, "");
+                }
+                String[] numbers = lore.split(" ");
+                value = numbers[2];
+            } else continue;
+
+            try {
+                int count = Integer.parseInt(value);
+                System.out.println("Updating " + stack + " to " + count);
+                stack.setCount(count == 0 ? 1 : count);
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
-            String[] numbers = lore.split(" ");
-            value = numbers[0];
-
-        } else if (name.contains("Profession")) { // Profession Icons
-            lore = lore.replaceAll(colourPattern, "");
-
-            String[] tokens = lore.split("Level: [0-9]{1,3}");
-            for (String token : tokens) {
-                lore = lore.replace(token, "");
-            }
-            String[] numbers = lore.split(" ");
-            value = numbers[1];
-        } else if (name.contains("'s Info")) { // Combat level on Info
-            lore = lore.replaceAll(colourPattern, "");
-
-            String[] tokens = lore.split("Combat Lv: [0-9]{1,3}");
-            for (String token : tokens) {
-                lore = lore.replace(token, "");
-            }
-            String[] numbers = lore.split(" ");
-            value = numbers[2];
-        } else return;
-
-        try {
-            int count = Integer.parseInt(value);
-            e.getGui().getSlotUnderMouse().getStack().setCount(count == 0 ? 1 : count);
-        } catch (Exception ex) {
-            ex.printStackTrace();
         }
     }
 
