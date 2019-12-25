8586af70a6190293caafeb969c9622c40ec03203
Fabio Augusto <21368488+HeyZeer0@users.noreply.github.com>
2019-12-25 14:18:55 -0300

    Fixed Health Regen id not showing properly

--- src/main/java/com/wynntils/core/framework/enums/SpellType.java
+++ src/main/java/com/wynntils/core/framework/enums/SpellType.java
@@ -8,10 +8,10 @@ import java.util.regex.Pattern;
 
 public enum SpellType {
 
-    FIRST_SPELL  ("1st Spell", "(Arrow Storm|Bolt Blizzard|Bash|Holy Blast|Heal|Remedy|Spin Attack|Whirlwind|Totem|Sky Emblem)"),
-    SECOND_SPELL ("2nd Spell", "(Escape|Spider Jump|Charge|Leap|Teleport|Blink|Vanish|Shadow Clone|Haul|Soar)"),
-    THIRD_SPELL  ("3rd Spell", "(Bomb(?: Arrow)?|Creeper Dart|Uppercut|Heaven Jolt|Meteor|Dead Star|Multi Hit|Leopard Punches|Aura|Wind Surge)"),
-    FOURTH_SPELL ("4th Spell", "(Arrow Shield|Dagger Aura|War Scream|Cry of the Gods|Ice Snake|Crystal Reptile|Smoke Bomb|Blinding Cloud|Uproot|Gale Funnel)");
+    FIRST_SPELL  ("1st Spell", "(Arrow Storm|Bolt Blizzard|Bash|Holy Blast|Heal|Remedy|Spin Attack|Whirlwind|Totem|Sky Emblem)\b"),
+    SECOND_SPELL ("2nd Spell", "(Escape|Spider Jump|Charge|Leap|Teleport|Blink|Vanish|Shadow Clone|Haul|Soar)\b"),
+    THIRD_SPELL  ("3rd Spell", "(Bomb(?: Arrow)?|Creeper Dart|Uppercut|Heaven Jolt|Meteor|Dead Star|Multi Hit|Leopard Punches|Aura|Wind Surge)\b"),
+    FOURTH_SPELL ("4th Spell", "(Arrow Shield|Dagger Aura|War Scream|Cry of the Gods|Ice Snake|Crystal Reptile|Smoke Bomb|Blinding Cloud|Uproot|Gale Funnel)\b");
 
     String shortName; Pattern regex;
 
