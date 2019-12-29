35ddcf443f563ae10b9dc443e22468329d4ff35a
Scyu_ <Scyu_@wynntils.com>
2019-12-29 23:39:45 +0000

    okay, now it's fixed...
    Signed-off-by: Scyu_ <Scyu_@wynntils.com>

--- src/main/java/com/wynntils/modules/core/events/ClientEvents.java
+++ src/main/java/com/wynntils/modules/core/events/ClientEvents.java
@@ -260,7 +260,7 @@ public class ClientEvents implements Listener {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void tickHandler(TickEvent.ClientTickEvent e) {
-        if (e.phase != TickEvent.Phase.END || !Reference.onWorld || !Reference.onLobby) return;
+        if (e.phase != TickEvent.Phase.END || (!Reference.onWorld && !Reference.onLobby)) return;
 
         EntityPlayer player = Minecraft.getMinecraft().player;
         int currentPosition = player.getPosition().getX() + player.getPosition().getY() + player.getPosition().getZ();
