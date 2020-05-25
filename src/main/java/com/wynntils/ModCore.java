94310a8924f0621b45175e57410a97a5259bf114
Magnus Ihse Bursie <mag@icus.se>
2020-05-25 02:29:20 +0200

    Accept -Dwynntils.development=true to set development environment (#188)
    * Accept -Dwynntils.development=true to set development environment
    
    * Cleaned up syntax
    
    Co-authored-by: Fabio Augusto <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/ModCore.java
+++ src/main/java/com/wynntils/ModCore.java
@@ -47,7 +47,7 @@ public class ModCore {
 
         jarFile = e.getSourceFile();
 
-        Reference.developmentEnvironment = (boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
+        Reference.developmentEnvironment = ((boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment")) || System.getProperty("wynntils.development").equals("true");
         // Reference.developmentEnvironment = false;  // Uncomment to test updater
 
         if (Reference.developmentEnvironment)
