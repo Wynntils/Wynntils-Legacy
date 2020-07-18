1ede91c8854d8d78329dbbebcfd9a5472fc2ea71
HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>
2020-07-18 11:51:36 -0300

    Fixed firefly night spawn
    Signed-off-by: HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/modules/visual/entities/conditions/FireflySpawnCondition.java
+++ src/main/java/com/wynntils/modules/visual/entities/conditions/FireflySpawnCondition.java
@@ -12,6 +12,7 @@ import com.wynntils.modules.visual.configs.VisualConfig;
 import com.wynntils.modules.visual.entities.EntityFirefly;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.init.Biomes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -23,9 +24,15 @@ public class FireflySpawnCondition implements EntitySpawnCodition {
 
     @Override
     public boolean shouldSpawn(Location pos, World world, EntityPlayerSP player, Random random) {
-        if (!VisualConfig.Fireflies.INSTANCE.enabled || world.isDaytime()) return false;
-        if (world.getBiome(pos.toBlockPos()) != Biomes.FOREST && world.getBiome(pos.toBlockPos()) != Biomes.SWAMPLAND) return false;
+        if (!VisualConfig.Fireflies.INSTANCE.enabled) return false;
+
+        BlockPos block = pos.toBlockPos();
+        if (world.getBiome(block) != Biomes.FOREST && world.getBiome(block) != Biomes.SWAMPLAND) return false;
         if (!LIGHT_FOREST.isInside(pos) && !DARK_FOREST.isInside(pos)) return false;
+
+        // Night starts at 12542 and ends at 23031
+        long worldTime = world.getWorldTime() % 24000;
+        if (worldTime < 12542 || worldTime > 23031) return false;
 
         return EntityFirefly.fireflies.get() < VisualConfig.Fireflies.INSTANCE.spawnLimit
                 && random.nextInt(VisualConfig.Fireflies.INSTANCE.spawnRate) == 0;
