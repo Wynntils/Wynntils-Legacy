9c8a5e1be4f71cc0d1bb4417b52c91c3847eef57
Jake <royalcows9@gmail.com>
2020-09-08 13:54:31 -0700

    Ear skins can now be loaded from any pair of locations on the skins file
    This ended up being a lot of work to do, however now ears can be loaded from any 8x8 location on the skins file, pre or post 1.8. More explination is in the LayerFoxEars file, and in the PR

--- src/main/java/com/wynntils/modules/cosmetics/layers/LayerFoxEars.java
+++ src/main/java/com/wynntils/modules/cosmetics/layers/LayerFoxEars.java
@@ -1,5 +1,24 @@
 /*
  *  * Copyright © Wynntils - 2018 - 2020.
+ *
+ * Updated 9/8/2020:
+ * Hey! Me (Cobular#5105) and Formuoli (formuoli#0719) just changed a bunch of stuff here to make ears more useful. Now,
+ * they support transparency, are rotated/mirrored correctly, and pull from a different place on the skins file. If you
+ * are reading this, you probably want to change this in some way. Feel free to @ us on the wynntils discord and we can
+ * help get you started. Remember to enable and disable blending though!
+ * Also, we're a python programmer and a C# programmer, so there may be some very non-java (read: inefficient and bad)
+ * ways to do things in here. Sorry about that, we'd be happy to learn what the correct way to do things may be!
+ *
+ * Changelog:
+ *  - Ears now support transparency
+ *  - Ears now are mirrored correctly in order to make them actually useful with transparency
+ *  - Added ability to load the ears from multiple parts of the skins file, since as far as we are aware, there is not a
+ *    14x7 block of pixels on the skin that allow transparency. The current implementation loads the two halves of the
+ *    ear texture (one 8x7, one 6x7 block, see https://docs.google.com/spreadsheets/d/1bLq-26mOE52BWusWeLWy6tvJmWfT73o3lCGFPZR40aM/edit#gid=0
+ *    for more information). As far as we know, the only valid pair of places for this are to the left and right of the
+ *    hat, which is what we are currently setup to use. This setup does not conflict with any other skin region in
+ *    vanilla. However, it is not backwards compatible with the old ears system (let's not kid ourselves though, no one
+ *    actually used that system, and this one's like a million times more useful)
  */
 
 package com.wynntils.modules.cosmetics.layers;
@@ -9,22 +28,22 @@ import com.wynntils.modules.core.instances.account.WynntilsUser;
 import com.wynntils.modules.core.managers.UserManager;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import com.wynntils.modules.cosmetics.layers.models.ExtendedModelRenderer;
 
 import static net.minecraft.client.renderer.GlStateManager.*;
 
 public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
 
     private final RenderPlayer playerRenderer;
-    private final ModelRenderer bipedFoxEarL;
-    private final ModelRenderer bipedFoxEarR;
+    private final ExtendedModelRenderer bipedFoxEarL;
+    private final ExtendedModelRenderer bipedFoxEarR;
 
     public LayerFoxEars(RenderPlayer playerRendererIn) {
         this.playerRenderer = playerRendererIn;
-        this.bipedFoxEarL = new ModelRenderer(playerRendererIn.getMainModel(), 50, 0);
-        this.bipedFoxEarR = new ModelRenderer(playerRendererIn.getMainModel(), 50, 0);
+        this.bipedFoxEarL = new ExtendedModelRenderer(playerRendererIn.getMainModel(), 32, 0, 56, 0);
+        this.bipedFoxEarR = new ExtendedModelRenderer(playerRendererIn.getMainModel(), 32, 0, 56, 0);
     }
 
     public void doRenderLayer(AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
@@ -38,20 +57,25 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
         float f1 = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * partialTicks;
 
 
-        pushMatrix(); {
+        pushMatrix();
+        enableBlend();
+        {
             rotate(f, 0.0F, 1.0F, 0.0F);
             rotate(f1, 1.0F, 0.0F, 0.0F);
             translate(0.375F * (float) (1 * 1.2 - 1), 0.0F, 0.0F);
             translate(0.0F, -0.5F, 0.0F); // Editing y here controls up-left
             rotate(-45f, 0.0F, 0.0F, 1.0F);
             translate(0.225F, 0.21F, 0.0F);  // -Z is forward, X is Up-Right, Y is down-right, -y is up-left
-
             scale(-1.3333334F, 1.3333334F, 1.3333334F); // Z is front to back, X seems right??
 
             renderModelR(player, playerRenderer.getMainModel(), 0.0625f);
-        } popMatrix();
+        }
+        disableBlend();
+        popMatrix();
 
-        pushMatrix(); {
+        pushMatrix();
+        enableBlend();
+        {
             rotate(f, 0.0F, 1.0F, 0.0F);
             rotate(f1, 1.0F, 0.0F, 0.0F);
             translate(0.375F * (float) (0 * 1.2 - 1), 0.0F, 0.0F);
@@ -60,16 +84,18 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
             scale(1.3333334F, 1.3333334F, 1.3333334F);
 
             renderModelL(player, playerRenderer.getMainModel(), 0.0625f);
-        } popMatrix();
+        }
+        disableBlend();
+        popMatrix();
     }
 
     public void renderModelL(AbstractClientPlayer player, ModelBase model, float scale) {
         bipedFoxEarL.cubeList.clear();
         if (player.isSneaking()) {
-            bipedFoxEarL.addBox(0.0F + 4F, -5.0F + 5F / 2F, 1f, 6, 6, 1);
+            bipedFoxEarL.addEars(0.0F + 4F, -5.0F + 5F / 2F, 1f, 6, 6, 1);
 
         } else {
-            bipedFoxEarL.addBox(0.0F, -5.0F, -1.0F, 6, 6, 1);
+            bipedFoxEarL.addEars(0.0F, -5.0F, -1.0F, 6, 6, 1);
         }
 
         ModelBase.copyModelAngles(playerRenderer.getMainModel().bipedHeadwear, bipedFoxEarL);
@@ -84,10 +110,10 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
     public void renderModelR(AbstractClientPlayer player, ModelBase model, float scale) {
         bipedFoxEarR.cubeList.clear();
         if (player.isSneaking()) {
-            bipedFoxEarR.addBox(0.0F + 4F, -5.0F + 5F / 2F, 1f, 6, 6, 1);
+            bipedFoxEarR.addEars(0.0F + 4F, -5.0F + 5F /  2F, 1f, 6, 6, 1);
 
         } else {
-            bipedFoxEarR.addBox(0.0F, -5.0F, -1.0F, 6, 6, 1);
+            bipedFoxEarR.addEars(0.0F, -5.0F, -1.0F, 6, 6, 1);
         }
 
 
@@ -96,7 +122,8 @@ public class LayerFoxEars implements LayerRenderer<AbstractClientPlayer> {
         bipedFoxEarR.rotateAngleY = playerRenderer.getMainModel().bipedHeadwear.rotateAngleY * (scale/2);
         bipedFoxEarR.rotateAngleZ = playerRenderer.getMainModel().bipedHeadwear.rotateAngleZ * (scale/2);
 
-//        ReflectionFields.ModelRenderer_compiled.setValue(bipedFoxEarR, false);
+// Why is this commented? Why does it work? Come back next week for answers to these dire questions and more!!
+        ReflectionFields.ModelRenderer_compiled.setValue(bipedFoxEarR, false);
         bipedFoxEarR.render(scale/2);
     }
 
--- src/main/java/com/wynntils/modules/cosmetics/layers/models/ExtendedModelBox.java
+++ src/main/java/com/wynntils/modules/cosmetics/layers/models/ExtendedModelBox.java
@@ -1,6 +1,140 @@
 package com.wynntils.modules.cosmetics.layers.models;
 
-import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.model.*;
 
-public class ExtendedModelBox extends ModelBox {
+public class ExtendedModelBox extends ModelBox
+{
+    /** The (x,y,z) vertex positions and (u,v) texture coordinates for each of the 8 points on a cube */
+    private PositionTextureVertex[] _vertexPositions;
+    /** An array of 6 TexturedQuads, one for each face of a cube */
+    private TexturedQuad[] quadList;
+    /** X vertex coordinate of lower box corner */
+    public float posX1;
+    /** Y vertex coordinate of lower box corner */
+    public float posY1;
+    /** Z vertex coordinate of lower box corner */
+    public float posZ1;
+    /** X vertex coordinate of upper box corner */
+    public float posX2;
+    /** Y vertex coordinate of upper box corner */
+    public float posY2;
+    /** Z vertex coordinate of upper box corner */
+    public float posZ2;
+    public String boxName;
+
+
+    public ExtendedModelBox (ModelRenderer renderer, int texU, int texV, float x, float y, float z, int dx, int dy, int dz, float delta)
+    {
+        super(renderer, texU, texV, x, y, z, dx, dy, dz, delta, renderer.mirror);
+    }
+
+    /** This exists in order to allow for a ModelBox to be textured from two separate areas of the skin, in order to allow for the use of more of the limited areas of transparency on the skin file.
+     * This will only work on DZ=1,2 otherwise things may not work as intended. Basically,
+     * @param renderer mirrored or not mirrored? not sure what this means.
+     * @param texOffsetX1 The X coord for the top left corner of the first of the two areas on the texture
+     * @param texOffsetY1 The Y coord for the top left corner of the first of the two areas on the texture
+     * @param texOffsetX2 The X coord for the top left corner of the second of the two areas on the texture
+     * @param texOffsetY2 The Y coord for the top left corner of the second of the two areas on the texture
+     * @param x offset. For solving vertex positions, ignore this (maybe something to do with position on character model?)
+     * @param y offset. For solving vertex positions, ignore this (maybe something to do with position on character model?)
+     * @param z offset. For solving vertex positions, ignore this (maybe something to do with position on character model?)
+     * @param dx Width of the overall box
+     * @param dy Height of the overall box
+     * @param dz Depth of the overall box
+     * @param delta Good f--king question.
+     */
+    public ExtendedModelBox(ExtendedModelRenderer renderer, int texOffsetX1, int texOffsetY1, int texOffsetX2, int texOffsetY2, float x, float y, float z, int dx, int dy, int dz, float delta, boolean mirror)
+    {
+        this(renderer.returnRenderer(), texOffsetX1, texOffsetY1, x, y, z, dx, dy, dz, delta);
+
+        this.posX1 = x;
+        this.posY1 = y;
+        this.posZ1 = z;
+        this.posX2 = x + (float)dx;
+        this.posY2 = y + (float)dy;
+        this.posZ2 = z + (float)dz;
+        this._vertexPositions = new PositionTextureVertex[8];
+        this.quadList = new TexturedQuad[6];
+        float f = x + (float)dx;
+        float f1 = y + (float)dy;
+        float f2 = z + (float)dz;
+        x = x - delta;
+        y = y - delta;
+        z = z - delta;
+        f = f + delta;
+        f1 = f1 + delta;
+        f2 = f2 + delta;
+
+//        if (mirror)
+//        {
+//            float f3 = f;
+//            f = x;
+//            x = f3;
+//        }
+
+        // I hope to god this isn't relevant for this
+        PositionTextureVertex positiontexturevertex7 = new PositionTextureVertex(x, y, z, 0.0F, 0.0F);
+        PositionTextureVertex positiontexturevertex = new PositionTextureVertex(f, y, z, 0.0F, 8.0F);
+        PositionTextureVertex positiontexturevertex1 = new PositionTextureVertex(f, f1, z, 8.0F, 8.0F);
+        PositionTextureVertex positiontexturevertex2 = new PositionTextureVertex(x, f1, z, 8.0F, 0.0F);
+        PositionTextureVertex positiontexturevertex3 = new PositionTextureVertex(x, y, f2, 0.0F, 0.0F);
+        PositionTextureVertex positiontexturevertex4 = new PositionTextureVertex(f, y, f2, 0.0F, 8.0F);
+        PositionTextureVertex positiontexturevertex5 = new PositionTextureVertex(f, f1, f2, 8.0F, 8.0F);
+        PositionTextureVertex positiontexturevertex6 = new PositionTextureVertex(x, f1, f2, 8.0F, 0.0F);
+        this._vertexPositions[0] = positiontexturevertex7;
+        this._vertexPositions[1] = positiontexturevertex;
+        this._vertexPositions[2] = positiontexturevertex1;
+        this._vertexPositions[3] = positiontexturevertex2;
+        this._vertexPositions[4] = positiontexturevertex3;
+        this._vertexPositions[5] = positiontexturevertex4;
+        this._vertexPositions[6] = positiontexturevertex5;
+        this._vertexPositions[7] = positiontexturevertex6;
+
+        // Ok, so here it uses the information about the coords to pull data from the skin to texture the model.
+        // The math is really cool, but a bit convoluted. Here is a spreadsheet that you can use to mess with it
+        // interactively: https://docs.google.com/spreadsheets/d/1bLq-26mOE52BWusWeLWy6tvJmWfT73o3lCGFPZR40aM/edit?usp=sharing
+        // https://l.jakecover.me/wynntils-extended-model-box-sheet
+
+        // x1y1 based
+        this.quadList[0] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex4, positiontexturevertex, positiontexturevertex1, positiontexturevertex5},
+                texOffsetX1 + dz + dx, texOffsetY1 + dz, texOffsetX1 + dz + dx + dz, texOffsetY1 + dz + dy, renderer.textureWidth, renderer.textureHeight);
+        this.quadList[1] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex7, positiontexturevertex3, positiontexturevertex6, positiontexturevertex2},
+                texOffsetX1, texOffsetY1 + dz, texOffsetX1 + dz, texOffsetY1 + dz + dy, renderer.textureWidth, renderer.textureHeight);
+        this.quadList[2] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex4, positiontexturevertex3, positiontexturevertex7, positiontexturevertex},
+                texOffsetX1 + dz, texOffsetY1, texOffsetX1 + dz + dx, texOffsetY1 + dz, renderer.textureWidth, renderer.textureHeight);
+        this.quadList[4] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex, positiontexturevertex7, positiontexturevertex2, positiontexturevertex1},
+                texOffsetX1 + dz, texOffsetY1 + dz, texOffsetX1 + dz + dx, texOffsetY1 + dz + dy, renderer.textureWidth, renderer.textureHeight);
+
+        // x2y2 based
+        this.quadList[3] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex1, positiontexturevertex2, positiontexturevertex6, positiontexturevertex5},
+                texOffsetX2, texOffsetY2, texOffsetX2 + dx, texOffsetY2 + dz, renderer.textureWidth, renderer.textureHeight);
+        this.quadList[5] = new TexturedQuad(new PositionTextureVertex[] {positiontexturevertex3, positiontexturevertex4, positiontexturevertex5, positiontexturevertex6},
+                texOffsetX2, texOffsetY2 + dz, texOffsetX2 + dx, texOffsetY1 + dz + dy, renderer.textureWidth, renderer.textureHeight);
+
+//        if (mirror)
+//        {
+//            for (TexturedQuad texturedquad : this.quadList)
+//            {
+//                texturedquad.flipFace();
+//            }
+//        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void render(BufferBuilder renderer, float scale)
+    {
+        for (TexturedQuad texturedquad : this.quadList)
+        {
+            texturedquad.draw(renderer, scale);
+        }
+    }
+
+    public ExtendedModelBox setBoxName(String name)
+    {
+        this.boxName = name;
+        return this;
+    }
 }
