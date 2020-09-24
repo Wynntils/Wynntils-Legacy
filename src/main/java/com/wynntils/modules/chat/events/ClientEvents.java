55131459f5ee715574c8f6c02635516c38c36c5f
P0ke <alextodaro@rocketmail.com>
2020-09-24 04:55:22 -0700

    Fix /gu defend getting replaced with garbage

--- src/main/java/com/wynntils/modules/chat/events/ClientEvents.java
+++ src/main/java/com/wynntils/modules/chat/events/ClientEvents.java
@@ -75,7 +75,7 @@ public class ClientEvents implements Listener {
     public void commandReplacements(ClientChatEvent e) {
         if (e.getMessage().startsWith("/tell")) e.setMessage(e.getMessage().replaceFirst("/tell", "/msg"));
         else if (e.getMessage().startsWith("/xp")) e.setMessage(e.getMessage().replaceFirst("/xp", "/guild xp"));
-        else if (e.getMessage().startsWith("/gu")) e.setMessage(e.getMessage().replaceFirst("att$", "attack").replaceFirst("a$", "attack").replaceFirst("def$", "defend").replaceFirst("d$", "defend").replaceFirst("c$", "contribute"));
+        else if (e.getMessage().startsWith("/gu")) e.setMessage(e.getMessage().replaceFirst("att$", "attack").replaceFirst("a$", "attack").replaceFirst("def$", "defend").replaceFirst(" d$", " defend").replaceFirst("c$", "contribute"));
         else if (e.getMessage().startsWith("/pa")) e.setMessage(e.getMessage().replaceFirst(" j ", " join ").replaceFirst(" i ", " invite ").replaceFirst("l$", "leave").replaceFirst("c$", "create"));
     }
 
