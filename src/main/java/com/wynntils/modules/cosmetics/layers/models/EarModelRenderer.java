52263def920d73525c9a0284591931a39332d837
Jake Cover <22972550+JakeCover@users.noreply.github.com>
2020-09-25 05:03:09 -0700

    Overhauled ears to support transparency and proper mirroring (#250)
    * Boy howdy fox ears, now whatever ears! They now support transparency and are mirrored correctly.
    
    These are really something. 7 or so hours later, me and formuoli have figured out that needed to make each ear a separate Box, how to wrangle the OpenGL matrices,  learned how skins work and where transparency is, and that no one can see the bottom of your neck. 
    
    Basically, the only downside to this whole thing is that it uses up a few pixels on the bottom of the neck hat, which is hardly visible to start with, but we have a patch in the works to fix this. 
    
    This has been a really cool challenge for some bored programmers, I hope this can be used by everyone!
    
    * Ear skins can now be loaded from any pair of locations on the skins file
    
    This ended up being a lot of work to do, however now ears can be loaded from any 8x8 location on the skins file, pre or post 1.8. More explanation is in the LayerFoxEars file, and in the PR
    
    * Reverting unnecessary gitignore changes
    
    Co-authored-by: HeyZeer0 <21368488+HeyZeer0@users.noreply.github.com>

---
+++ src/main/java/com/wynntils/modules/cosmetics/layers/models/EarModelRenderer.java
@@ -0,0 +1,55 @@
+package com.wynntils.modules.cosmetics.layers.models;
+
+import com.google.common.collect.Lists;
+
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+
+public class EarModelRenderer extends ModelRenderer {
+
+    /** The first X offset into the texture used for displaying this model */
+    private int textureOffsetX1;
+    /** The first Y offset into the texture used for displaying this model */
+    private int textureOffsetY1;
+    /** The second X offset into the texture used for displaying this model */
+    private int textureOffsetX2;
+    /** The second Y offset into the texture used for displaying this model */
+    private int textureOffsetY2;
+
+
+    public EarModelRenderer(ModelBase model, String boxNameIn) {
+        super(model, boxNameIn);
+
+        this.cubeList = Lists.newArrayList();
+    }
+
+    public EarModelRenderer(ModelBase model)
+    {
+        this(model, null);
+    }
+
+    public EarModelRenderer(ModelBase model, int texOffX1, int texOffY1, int texOffX2, int texOffY2) {
+        this(model);
+
+        this.setTextureOffset(texOffX1, texOffY1, texOffX2, texOffY2);
+    }
+
+    public EarModelRenderer setTextureOffset(int x1, int y1, int x2, int y2) {
+        this.textureOffsetX1 = x1;
+        this.textureOffsetY1 = y1;
+        this.textureOffsetX2 = x2;
+        this.textureOffsetY2 = y2;
+        return this;
+    }
+
+    public EarModelRenderer addEars(float offX, float offY, float offZ, int width, int height, int depth) {
+        this.cubeList.add(new EarModelBox(this, this.textureOffsetX1, this.textureOffsetY1, this.textureOffsetX2, this.textureOffsetY2, offX, offY, offZ, width, height, depth, 0.0f, false));
+        return this;
+    }
+
+    public ModelRenderer returnRenderer() {
+        return this;
+    }
+
+}
