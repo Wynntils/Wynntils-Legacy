32e6f15951991dfbc979e564db79ae225087092c
Lukas <69743171+LukasmanMHdude@users.noreply.github.com>
2021-01-01 12:13:47 -0800

    fix brackets

--- src/main/java/com/wynntils/modules/utilities/overlays/OverlayEvents.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/OverlayEvents.java
@@ -370,10 +370,10 @@ public class OverlayEvents implements Listener {
                 GameUpdateOverlay.queueMessage(TextFormatting.GRAY + "Not enough inventory space.");
                 e.setCanceled(true);
                 return;
-            } else if (messageText.equals("Select a class! Each class is saved individually across all servers, you can come back at any time with /class and select another class!")) 
+            } else if (messageText.equals("Select a class! Each class is saved individually across all servers, you can come back at any time with /class and select another class!")) {
                 e.setCanceled(true);
                 return;
-            } else if (messageText.equals("Thank you for using the WynnPack. Enjoy the game!")) 
+            } else if (messageText.equals("Thank you for using the WynnPack. Enjoy the game!")) {
                 e.setCanceled(true);
                 return;
             } else if (messageText.equals("Loading Resource Pack...")) {
