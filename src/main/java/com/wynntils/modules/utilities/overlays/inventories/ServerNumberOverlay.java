c06172376233ede775c9b13cb327b83198d00d33
DonkeyBlaster69 <ryanzhou2004@gmail.com>
2021-02-23 11:58:43 -0800

    Removed useless config, replaced assert, changed item name checking to pattern

--- src/main/java/com/wynntils/modules/utilities/overlays/inventories/ServerNumberOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/inventories/ServerNumberOverlay.java
@@ -6,34 +6,31 @@ package com.wynntils.modules.utilities.overlays.inventories;
 
 import com.wynntils.core.events.custom.RenderEvent;
 import com.wynntils.core.framework.interfaces.Listener;
-import com.wynntils.modules.utilities.configs.UtilitiesConfig;
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ServerNumberOverlay implements Listener {
+
+    private static final Pattern WORLD_PATTERN = Pattern.compile("World (\\d+)?");
 
     @SubscribeEvent
     public void onItemOverlay(RenderEvent.DrawItemOverlay event) {
-        if (!UtilitiesConfig.Items.INSTANCE.itemLevelOverlayOutsideGui && Minecraft.getMinecraft().currentScreen == null) return;
 
         ItemStack serverStack = event.getStack();
         String serverName = TextFormatting.getTextWithoutFormattingCodes(serverStack.getDisplayName());
 
-        assert serverName != null;
-        if (!serverName.startsWith("World")) return;
-        // If the "items" don't start with "World", assume we aren't in the server selector and just don't play with the numbers
-        // There is probably a better way to do this, but I can't get the container title "Wynncraft Servers" in this event, so here we are
+        if (serverName == null) return;
 
-        // server number replacements
-        try {
-            int serverNumber = Integer.parseInt(serverName.replaceAll("[^0-9]+", ""));
-            event.setOverlayText(String.valueOf(serverNumber));
-        } catch (NumberFormatException ignored) {
-            // I know ignoring exceptions is probably not good, but I don't see a reason to throw some error or two into the user's logs
-            // This is only triggered when it scans for server names, looking for integers
-        }
+        // Check if item follows pattern "World #"
+        Matcher m = WORLD_PATTERN.matcher(serverName);
+        if (!m.lookingAt()) return;
+
+        int serverNumber = Integer.parseInt(serverName.replaceAll("[^0-9]+", ""));
+        event.setOverlayText(String.valueOf(serverNumber));
     }
 
 }
