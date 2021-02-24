4ec4f8073c8c85141c8508c9d3fa8654f1e0bab3
DonkeyBlaster69 <ryanzhou2004@gmail.com>
2021-02-23 16:00:35 -0800

    Cleaned up method for finding server items and numbers

--- src/main/java/com/wynntils/modules/utilities/overlays/inventories/ServerNumberOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/inventories/ServerNumberOverlay.java
@@ -6,7 +6,9 @@ package com.wynntils.modules.utilities.overlays.inventories;
 
 import com.wynntils.core.events.custom.RenderEvent;
 import com.wynntils.core.framework.interfaces.Listener;
+import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -15,7 +17,7 @@ import java.util.regex.Pattern;
 
 public class ServerNumberOverlay implements Listener {
 
-    private static final Pattern WORLD_PATTERN = Pattern.compile("World (\\d+)?");
+    private static final Pattern WORLD_PATTERN = Pattern.compile("World (\\d+)(?: \\(Recommended\\))?");
 
     @SubscribeEvent
     public void onItemOverlay(RenderEvent.DrawItemOverlay event) {
@@ -25,12 +27,11 @@ public class ServerNumberOverlay implements Listener {
 
         if (serverName == null) return;
 
-        // Check if item follows pattern "World #"
+        // Check if item follows pattern "World #" or "World # (Recommended)"
         Matcher m = WORLD_PATTERN.matcher(serverName);
-        if (!m.lookingAt()) return;
+        if (!m.matches()) return;
 
-        int serverNumber = Integer.parseInt(serverName.replaceAll("[^0-9]+", ""));
-        event.setOverlayText(String.valueOf(serverNumber));
+        event.setOverlayText(m.group(1));
     }
 
 }
