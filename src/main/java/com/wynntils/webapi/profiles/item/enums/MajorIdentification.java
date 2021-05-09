ce0eb9ec95c46786282f9cbb8b9cbf8d8667922a
Danny van Dortmont <35771251+HighCrit@users.noreply.github.com>
2021-05-09 15:54:34 +0200

    Added FLASHFREEZE to major IDs

--- src/main/java/com/wynntils/webapi/profiles/item/enums/MajorIdentification.java
+++ src/main/java/com/wynntils/webapi/profiles/item/enums/MajorIdentification.java
@@ -28,6 +28,7 @@ public enum MajorIdentification {
     CHERRY_BOMBS("Cherry Bombs", "Your Smoke Bombs explode instantly on contact, dealing 110% damage each"),
     PEACEFUL_EFFIGY("Peaceful Effigy", "Your Totem will last twice as long"),
     FURIOUS_EFFIGY("Furious Effigy", "Totem effects are twice as fast, but duration is halved");
+    FLASHFREEZE("Flash Freeze", "Ice Snake is instant but has a reduced range");
 
     String name, description;
 
