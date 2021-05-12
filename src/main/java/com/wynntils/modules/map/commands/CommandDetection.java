433304f008d282ff9e7837d7821b3a6e53ef2517
Magnus Ihse Bursie <mag@icus.se>
2021-05-12 23:27:19 +0200

    Rounding error in the NPC detector

--- src/main/java/com/wynntils/modules/map/commands/CommandDetection.java
+++ src/main/java/com/wynntils/modules/map/commands/CommandDetection.java
@@ -116,7 +116,7 @@ public class CommandDetection extends CommandBase implements IClientCommand {
 
     private void printInstance(PrintStream ps, String type, String name, String formattedName, Location key) {
         // Write a CSV line
-        ps.println(type + ", " + name + ", " + formattedName + ", " + (int) Math.round(key.x) + ", " + (int) Math.round(key.y) + ", " + (int) Math.round(key.z));
+        ps.println(type + ", " + name + ", " + formattedName + ", " + (int) Math.floor(key.x) + ", " + (int) Math.floor(key.y) + ", " + (int) Math.floor(key.z));
     }
 
 }
