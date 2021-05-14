66bb2416b036f58721fedc1d75be9485f2decb0c
Magnus Ihse Bursie <mag@icus.se>
2021-05-14 14:07:56 +0200

    Explain purpose of McIf

--- src/main/java/com/wynntils/McIf.java
+++ src/main/java/com/wynntils/McIf.java
@@ -1,7 +1,17 @@
+/*
+ *  * Copyright © Wynntils - 2021.
+ */
+
 package com.wynntils;
 
 import net.minecraft.util.text.ITextComponent;
 
+/**
+ * The Wynntils Minecraft Interface (MC IF).
+ *
+ * This class wraps all Minecraft functionality that we need but do not want to
+ * depend on directly, for instance due to version disparity.
+ */
 public class McIf {
     public static String getUnformattedText(ITextComponent msg) {
         return msg.getUnformattedText();
