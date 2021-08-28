863e7d97cd2d79cb03eea8d4aafb8f799d52e03d
P0ke <3767283+P0keDev@users.noreply.github.com>
2021-08-28 15:00:21 -0700

    Fix sea skipper ui crash

--- src/main/java/com/wynntils/modules/map/overlays/ui/SeaskipperWorldMapUI.java
+++ src/main/java/com/wynntils/modules/map/overlays/ui/SeaskipperWorldMapUI.java
@@ -154,7 +154,7 @@ public class SeaskipperWorldMapUI extends WorldMapUI {
                 location.setCost(cost);
                 location.setActiveType(SeaskipperLocation.Accessibility.ACCESSIBLE);
                 routes.put(location, s.slotNumber);
-                return;
+                continue;
             }
 
             if (stack.getItem() == Items.BOAT) { //Buy boat
