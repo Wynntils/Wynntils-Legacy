74df7a2519d0cc64ca44a37d724366c2771a06f7
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2021-10-10 11:08:52 -0700

    Healing Pot FIx

--- src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
+++ src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
@@ -829,12 +829,17 @@ public class ClientEvents implements Listener {
         UtilitiesConfig.INSTANCE.saveSettings(UtilitiesModule.getModule());
     }
 
+    private static final Pattern potionRegex = Pattern.compile("Potions? of Healing");
+
     // blocking healing pots below
     @SubscribeEvent
     public void onUseItem(PacketEvent<CPacketPlayerTryUseItem> e) {
         ItemStack item = McIf.player().getHeldItem(EnumHand.MAIN_HAND);
 
-        if (item.isEmpty() || !item.hasDisplayName() || !item.getDisplayName().contains(TextFormatting.RED + "Potion of Healing") || !UtilitiesConfig.INSTANCE.blockHealingPots) return;
+        if (item.isEmpty() || !item.hasDisplayName() || !UtilitiesConfig.INSTANCE.blockHealingPots) return;
+
+        if (item.getDisplayName().startsWith(TextFormatting.DARK_AQUA.toString())) return;
+        if (!item.getDisplayName().contains(TextFormatting.LIGHT_PURPLE + "Potions of Healing") && !item.getDisplayName().contains(TextFormatting.RED + "Potions of Healing")) return;
 
         EntityPlayerSP player = McIf.player();
         if (player.getHealth() != player.getMaxHealth()) return;
