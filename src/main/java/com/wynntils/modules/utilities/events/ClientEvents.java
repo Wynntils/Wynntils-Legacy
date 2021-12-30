4ac29767e24b9d66c967af12f75f10f796c1d3f5
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2021-12-30 06:28:44 -0800

    fix: Bulk buy check fixes (#400)
    * Add TM check
    
    * Fix Ava workshop consumables check

--- src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
+++ src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
@@ -1078,10 +1078,12 @@ public class ClientEvents implements Listener {
         String itemName = is.getDisplayName();
         return (itemName.endsWith(" Teleport Scroll") ||
                 itemName.contains("Potion of ") || // We're using .contains here because we check for skill point potions which are different colors/symbols
-                itemName.endsWith("Speed Surge") ||
-                itemName.endsWith("Bipedal Spring"))
+                itemName.endsWith("Speed Surge [1/1]") ||
+                itemName.endsWith("Bipedal Spring [1/1]"))
 
-                && ItemUtils.getStringLore(is).contains("§6Price:");
+                && ItemUtils.getStringLore(is).contains("§6Price:")
+                && !ItemUtils.getStringLore(is).contains(" x "); // Make sure we're not in trade market
+        // Normal shops don't have a string with " x " whereas TM uses it for the amount of the item being sold
     }
 
     @SubscribeEvent
