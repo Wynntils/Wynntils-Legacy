169b6d6120e2fcddeee4a24383a3bba4f6cd2300
kristofbolyai <bolyai.bolyai@gmail.com>
2021-12-31 15:25:44 +0100

    Added checks to spellcasting: is current item a weapon, is combat level reached, are spell point minimums reached?

--- src/main/java/com/wynntils/modules/utilities/managers/QuickCastManager.java
+++ src/main/java/com/wynntils/modules/utilities/managers/QuickCastManager.java
@@ -13,7 +13,6 @@ import com.wynntils.core.framework.instances.data.CharacterData;
 import com.wynntils.core.framework.instances.data.SpellData;
 import com.wynntils.core.utils.ItemUtils;
 import com.wynntils.modules.core.managers.PacketQueue;
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketAnimation;
@@ -28,6 +27,10 @@ import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static com.wynntils.core.framework.instances.data.SpellData.SPELL_LEFT;
 import static com.wynntils.core.framework.instances.data.SpellData.SPELL_RIGHT;
 
@@ -38,6 +41,11 @@ public class QuickCastManager {
     private static final CPacketPlayerDigging releaseClick = new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN);
 
     private static final int[] spellUnlock = { 1, 11, 21, 31 };
+
+    private static final Pattern WEAPON_SPEED_PATTERN = Pattern.compile("§7.+ Attack Speed");
+    private static final Pattern CLASS_REQ_OK_PATTERN = Pattern.compile("§a✔§7 Class Req:.+");
+    private static final Pattern COMBAT_LVL_REQ_OK_PATTERN = Pattern.compile("§a✔§7 Combat Lv. Min:.+");
+    private static final Pattern SPELL_POINT_MIN_NOT_REACHED_PATTERN = Pattern.compile("§c✖§7 (.+) Min: (\\d+)");
 
     private static void queueSpell(int spellNumber, boolean a, boolean b, boolean c) {
         if (!canCastSpell(spellNumber)) return;
@@ -51,7 +59,6 @@ public class QuickCastManager {
     }
 
     public static void castFirstSpell() {
-        if (!ItemUtils.getStringLore(McIf.player().getHeldItemMainhand()).contains("§7 Class Req:")) return;
         if (PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER) {
             queueSpell(1, SPELL_LEFT, SPELL_RIGHT, SPELL_LEFT);
             return;
@@ -61,7 +68,6 @@ public class QuickCastManager {
     }
 
     public static void castSecondSpell() {
-        if (!ItemUtils.getStringLore(McIf.player().getHeldItemMainhand()).contains("§7 Class Req:")) return;
         if (PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER) {
             queueSpell(2, SPELL_LEFT, SPELL_LEFT, SPELL_LEFT);
             return;
@@ -71,7 +77,6 @@ public class QuickCastManager {
     }
 
     public static void castThirdSpell() {
-        if (!ItemUtils.getStringLore(McIf.player().getHeldItemMainhand()).contains("§7 Class Req:")) return;
         if (PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER) {
             queueSpell(3, SPELL_LEFT, SPELL_RIGHT, SPELL_RIGHT);
             return;
@@ -81,7 +86,6 @@ public class QuickCastManager {
     }
 
     public static void castFourthSpell() {
-        if (!ItemUtils.getStringLore(McIf.player().getHeldItemMainhand()).contains("§7 Class Req:")) return;
         if (PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER) {
             queueSpell(4, SPELL_LEFT, SPELL_LEFT, SPELL_RIGHT);
             return;
@@ -98,6 +102,91 @@ public class QuickCastManager {
         if (PlayerInfo.get(CharacterData.class).getLevel() < spellUnlock[spell - 1]) {
             McIf.player().sendMessage(new TextComponentString(
                     TextFormatting.GRAY + "You have not yet unlocked this spell! You need to be level " + spellUnlock[spell - 1]
+            ));
+            return false;
+        }
+
+        if (PlayerInfo.get(CharacterData.class).getCurrentMana() == 0) {
+            McIf.player().sendMessage(new TextComponentString(
+                    TextFormatting.GRAY + "You do not have enough mana to cast this spell!"
+            ));
+            return false;
+        }
+
+        ItemStack heldItem = McIf.player().getHeldItemMainhand();
+
+        List<String> lore = ItemUtils.getLore(heldItem);
+
+        //If item has attack speed line, it is a weapon
+        boolean isWeapon = false;
+        //Check class reqs to see if the weapon can be used by current class
+        boolean classReqOk = false;
+        //Is the current combat level enough to use the weapon
+        boolean combatLvlMinReached = false;
+        //If there is a spell point requirement that is not reached, store it and print it later
+        String notReachedSpellPointRequirements = null;
+
+        int i = 0;
+        for (; i < lore.size(); i++) {
+            if (WEAPON_SPEED_PATTERN.matcher(lore.get(i)).matches())
+            {
+                isWeapon = true;
+                break;
+            }
+        }
+
+        if (!isWeapon)
+        {
+            McIf.player().sendMessage(new TextComponentString(
+                    TextFormatting.GRAY + "The held item is not a weapon."
+            ));
+            return false;
+        }
+
+        for (; i < lore.size(); i++) {
+            if (CLASS_REQ_OK_PATTERN.matcher(lore.get(i)).matches())
+            {
+                classReqOk = true;
+                break;
+            }
+        }
+
+        if (!classReqOk)
+        {
+            McIf.player().sendMessage(new TextComponentString(
+                    TextFormatting.GRAY + "The held weapon is not for this class."
+            ));
+            return false;
+        }
+
+        for (; i < lore.size(); i++) {
+            if (COMBAT_LVL_REQ_OK_PATTERN.matcher(lore.get(i)).matches())
+            {
+                combatLvlMinReached = true;
+                break;
+            }
+        }
+
+        if (!combatLvlMinReached)
+        {
+            McIf.player().sendMessage(new TextComponentString(
+                    TextFormatting.GRAY + "The current class level is too low to use the held weapon."
+            ));
+            return false;
+        }
+
+        for (; i < lore.size(); i++) {
+            Matcher matcher = SPELL_POINT_MIN_NOT_REACHED_PATTERN.matcher(lore.get(i));
+            if (matcher.matches())
+            {
+                notReachedSpellPointRequirements = matcher.group(1);
+                break;
+            }
+        }
+
+        if (notReachedSpellPointRequirements != null) {
+            McIf.player().sendMessage(new TextComponentString(
+                    TextFormatting.GRAY + "The current class does not have enough "+notReachedSpellPointRequirements+" to use the held weapon."
             ));
             return false;
         }
