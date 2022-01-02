d7cce400ba11eb478dcb30a0377606b5eb9b2b3d
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-01-02 16:17:48 +0100

    Space🙃
    Co-authored-by: Fabio Augusto <21368488+HeyZeer0@users.noreply.github.com>

--- src/main/java/com/wynntils/modules/map/overlays/ui/MainWorldMapUI.java
+++ src/main/java/com/wynntils/modules/map/overlays/ui/MainWorldMapUI.java
@@ -36,7 +36,7 @@ public class MainWorldMapUI extends WorldMapUI {
     private long lastClickTime = Integer.MAX_VALUE;
     private static final long doubleClickTime = Utils.getDoubleClickTime();
 
-    //Only updated if MapConfig.WorldMap.INSTANCE.autoCloseMapOnMovement is true, don't rely on this for anything else
+    // Only updated if MapConfig.WorldMap.INSTANCE.autoCloseMapOnMovement is true, don't rely on this for anything else
     private double lastPlayerX = Double.MAX_VALUE;
     private double lastPlayerZ = Double.MAX_VALUE;
 
