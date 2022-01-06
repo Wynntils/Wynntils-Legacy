b8fced7c4385f7e0497734b8f28cb4ca7f769249
kristofbolyai <bolyai.bolyai@gmail.com>
2022-01-06 12:54:23 +0100

    Removed useless variables

--- src/main/java/com/wynntils/webapi/profiles/ingredient/IngredientProfile.java
+++ src/main/java/com/wynntils/webapi/profiles/ingredient/IngredientProfile.java
@@ -1,8 +1,5 @@
 package com.wynntils.webapi.profiles.ingredient;
 
-import com.wynntils.core.framework.ui.elements.UIEList;
-import com.wynntils.core.utils.StringUtils;
-import com.wynntils.modules.questbook.overlays.ui.IngredientPage;
 import com.wynntils.modules.utilities.configs.UtilitiesConfig;
 import com.wynntils.webapi.WebManager;
 import com.wynntils.webapi.profiles.ingredient.enums.IngredientModifierType;
@@ -10,21 +7,15 @@ import com.wynntils.webapi.profiles.ingredient.enums.IngredientTier;
 import com.wynntils.webapi.profiles.ingredient.enums.ItemModifierType;
 import com.wynntils.webapi.profiles.ingredient.enums.ProfessionType;
 import com.wynntils.webapi.profiles.item.objects.IdentificationContainer;
-import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.text.TextFormatting;
 
-import javax.xml.soap.Text;
 import java.util.*;
-
-import static net.minecraft.util.text.TextFormatting.GREEN;
-import static net.minecraft.util.text.TextFormatting.RED;
 
 public class IngredientProfile {
 
@@ -39,8 +30,8 @@ public class IngredientProfile {
     Map<String, Integer> ingredientModifiers = new HashMap<>();
 
     transient IngredientTier ingredientTier;
-    transient List<ItemModifier> itemModifiersList = new ArrayList<>();
-    transient List<IngredientModifier> ingredientModifiersList = new ArrayList<>();
+    //transient List<ItemModifier> itemModifiersList = new ArrayList<>();
+    //transient List<IngredientModifier> ingredientModifiersList = new ArrayList<>();
     transient Map<ItemModifierType, ItemModifier> itemModifiersTypeMap = new HashMap<>();
     transient ItemStack guideStack;
 
@@ -51,24 +42,11 @@ public class IngredientProfile {
     public void postParse() {
         ingredientTier = IngredientTier.fromInteger(tier);
 
-        itemModifiersList = new ArrayList<>();
         itemModifiersTypeMap = new HashMap<>();
         for (String key : itemModifiers.keySet()) {
             ItemModifierType type = ItemModifierType.valueOf(key.toUpperCase(Locale.ROOT));
             ItemModifier itemModifier = new ItemModifier(type, itemModifiers.get(key));
-            itemModifiersList.add(itemModifier);
             itemModifiersTypeMap.put(type, itemModifier);
-        }
-
-        ingredientModifiersList = new ArrayList<>();
-        for (String key : ingredientModifiers.keySet()) {
-            IngredientModifierType type;
-            if (key.equals("notTouching"))
-                type = IngredientModifierType.NOT_TOUCHING;
-            else
-                type = IngredientModifierType.valueOf(key.toUpperCase(Locale.ROOT));
-
-            ingredientModifiersList.add(new IngredientModifier(type, ingredientModifiers.get(key)));
         }
     }
 
@@ -146,11 +124,17 @@ public class IngredientProfile {
         if (statuses.size() > 0)
             itemLore.add("");
 
-        for (IngredientModifier ingredientModifier : ingredientModifiersList) {
-            itemLore.addAll(Arrays.asList(ingredientModifier.getLoreLines()));
+        for (String key : ingredientModifiers.keySet()) {
+            IngredientModifierType type;
+            if (key.equals("notTouching"))
+                type = IngredientModifierType.NOT_TOUCHING;
+            else
+                type = IngredientModifierType.valueOf(key.toUpperCase(Locale.ROOT));
+
+            itemLore.addAll(Arrays.asList(new IngredientModifier(type, ingredientModifiers.get(key)).getLoreLines()));
         }
 
-        if (ingredientModifiersList.size() > 0)
+        if (ingredientModifiers.size() > 0)
             itemLore.add("");
 
         ItemModifier durability = itemModifiersTypeMap.get(ItemModifierType.DURABILITY);
@@ -163,13 +147,17 @@ public class IngredientProfile {
         else if (duration != null)
             itemLore.add(duration.getFormattedModifierText());
 
-        for (ItemModifier itemModifier : itemModifiersList) {
+
+        for (String key : itemModifiers.keySet()) {
+            ItemModifierType type = ItemModifierType.valueOf(key.toUpperCase(Locale.ROOT));
+            ItemModifier itemModifier = new ItemModifier(type, itemModifiers.get(key));
+
             if (itemModifier.type == ItemModifierType.DURABILITY || itemModifier.type == ItemModifierType.DURATION) continue;
 
             itemLore.add(itemModifier.getFormattedModifierText());
         }
 
-        if (durability != null || duration != null || itemModifiersList.size() > 0)
+        if (durability != null || duration != null || itemModifiers.size() > 0)
             itemLore.add("");
 
         // Bolyai, 2022.01.04: As of the date, untradeable ingredients don't exist, adding this for future
@@ -197,36 +185,28 @@ public class IngredientProfile {
             tag.setTag("display", display);
             tag.setBoolean("Unbreakable", true);  // this allow items to have damage
 
-
-            //FIXME: Fix item head textures
             if (stack.getItem() == Items.SKULL) {
                 HashMap<String, String> ingredientHeadTextures = WebManager.getIngredientHeadTextures();
 
                 if (ingredientHeadTextures.containsKey(name))
                 {
-                    NBTTagCompound skullOwner = new NBTTagCompound();
+                    NBTTagCompound skullData = new NBTTagCompound();
+                    skullData.setTag("Id", new NBTTagString(UUID.randomUUID().toString()));
 
-                    NBTTagCompound propertiesTag = new NBTTagCompound();
+                    NBTTagCompound properties = new NBTTagCompound();
+                    NBTTagList textures = new NBTTagList();
+                    NBTTagCompound textureEntry = new NBTTagCompound();
+                    textureEntry.setTag("Value", new NBTTagString(ingredientHeadTextures.get(name)));
+                    textures.appendTag(textureEntry);
+                    properties.setTag("textures", textures);
+                    skullData.setTag("Properties", properties);
 
-                    NBTTagList texturesTag = new NBTTagList();
-
-                    NBTTagCompound valueTag = new NBTTagCompound();
-
-                    valueTag.setString("Value", ingredientHeadTextures.get(name));
-
-                    texturesTag.appendTag(valueTag);
-
-                    propertiesTag.setTag("textures", texturesTag);
-
-                    skullOwner.setTag("Properties", propertiesTag);
-
-                    tag.setTag("SkullOwner", skullOwner);
+                    tag.setTag("SkullOwner", skullData);
                 }
             }
 
             stack.setTagCompound(tag);
         }
-
 
         return guideStack = stack;
     }
