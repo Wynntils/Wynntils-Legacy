62371974fa0a6953bc5601502ab387d7cd103788
DonkeyBlaster69 <57310593+DonkeyBlaster@users.noreply.github.com>
2022-01-11 14:11:59 -0800

    Add in identifier checks

--- src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
+++ src/main/java/com/wynntils/modules/utilities/events/ClientEvents.java
@@ -103,6 +103,8 @@ public class ClientEvents implements Listener {
     private GameUpdateOverlay.MessageContainer emeraldPouchMessage;
     private IInventory currentLootChest;
 
+    private boolean isInIdentifier = false;
+
     private static final Pattern PRICE_REPLACER = Pattern.compile("§6 - §a. §f([1-9]\\d*)§7" + EmeraldSymbols.E_STRING);
     private static final Pattern INGREDIENT_SPLIT_PATTERN = Pattern.compile("§f(\\d+) x (.+)");
     private static final Pattern WAR_CHAT_MESSAGE_PATTERN = Pattern.compile("§3\\[WAR§3\\] The war for (.+) will start in \\d+ minutes.");
@@ -283,6 +285,11 @@ public class ClientEvents implements Listener {
                 && ((ChestReplacer) e.getGui()).getLowerInv().getSizeInventory() == 27) {
             currentLootChest = ((ChestReplacer) e.getGui()).getLowerInv();
         }
+
+        if (e.getGui() instanceof ChestReplacer && (((ChestReplacer) e.getGui()).getLowerInv().getName()).contains("Item Identifier")
+                && ((ChestReplacer) e.getGui()).getLowerInv().getSizeInventory() == 9) {
+            isInIdentifier = true;
+        }
     }
 
   @SubscribeEvent
@@ -290,6 +297,7 @@ public class ClientEvents implements Listener {
         if (e.getGui() == null) {
             afkProtectionBlocked = false;
             lastUserInput = System.currentTimeMillis();
+            isInIdentifier = false;
         } else if (e.getGui() instanceof InventoryReplacer || e.getGui() instanceof ChestReplacer || e.getGui() instanceof HorseReplacer) {
             afkProtectionBlocked = true;
         }
