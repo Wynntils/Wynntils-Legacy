Wynntils/Wynntils-Legacy

commit 9cca50e43c3ab3c9b281b193bff1ac22dad3df96

Make dungeon key overlay use acronyms and fix normal keys not showing up on TM (#446)
Signed-off-by: kristofbolyai <bolyai.bolyai@gmail.com>

src/main/java/com/wynntils/modules/utilities/overlays/inventories/ItemSpecificationOverlay.java
@@ -94,15 +94,24 @@ public class ItemSpecificationOverlay implements Listener {
             if (UtilitiesConfig.Items.INSTANCE.keySpecification) {
                 Pattern dungeonKey = Pattern.compile("§6(.*) Key");
                 Matcher m = dungeonKey.matcher(name);
-                if (m.find() && lore.get(0).equals("§7Grants access to the")) {
-                    destinationName = m.group(1).substring(0, 1);
+                // Lore line is different on TM keys
+                if (m.find() && (lore.get(0).equals("§7Grants access to the") || lore.get(4).equals("§7Grants access to the"))) {
+                    StringBuilder builder = new StringBuilder();
+                    for (String part : m.group(1).split(" ")) {
+                        builder.append(part.charAt(0));
+                    }
+                    destinationName = builder.toString();
                     color = MinecraftChatColors.GOLD;
                 }
 
                 Pattern brokenDungeonKey = Pattern.compile("Broken (.*) Key");
                 Matcher m2 = brokenDungeonKey.matcher(name);
                 if (m2.find()) {
-                    destinationName = m2.group(1).substring(0, 1);
+                    StringBuilder builder = new StringBuilder();
+                    for (String part : m2.group(1).split(" ")) {
+                        builder.append(part.charAt(0));
+                    }
+                    destinationName = builder.toString();
                     color = MinecraftChatColors.DARK_RED;
                 }
 
@@ -110,7 +119,11 @@ public class ItemSpecificationOverlay implements Listener {
                 Pattern corruptedDungeonKey = Pattern.compile("§4(?:Broken )?(?:Corrupted )?(.*) Key");
                 Matcher m3 = corruptedDungeonKey.matcher(name);
                 if (m3.find()) {
-                    destinationName = m3.group(1).substring(0, 1);
+                    StringBuilder builder = new StringBuilder();
+                    for (String part : m3.group(1).split(" ")) {
+                        builder.append(part.charAt(0));
+                    }
+                    destinationName = builder.toString();
                     color = MinecraftChatColors.DARK_RED;
                 }
             }

commit b46c2152981fddea99308a2da5d89ac9025501de

fix ItemSpecificationOverlay
Caused by #446

src/main/java/com/wynntils/modules/utilities/overlays/inventories/ItemSpecificationOverlay.java
@@ -95,7 +95,7 @@ public class ItemSpecificationOverlay implements Listener {
                 Pattern dungeonKey = Pattern.compile("§6(.*) Key");
                 Matcher m = dungeonKey.matcher(name);
                 // Lore line is different on TM keys
-                if (m.find() && (lore.get(0).equals("§7Grants access to the") || lore.get(4).equals("§7Grants access to the"))) {
+                if (m.find() && lore.size() > 0 && (lore.get(0).equals("§7Grants access to the") || (lore.size() >= 4 && lore.get(4).equals("§7Grants access to the")))) {
                     StringBuilder builder = new StringBuilder();
                     for (String part : m.group(1).split(" ")) {
                         builder.append(part.charAt(0));