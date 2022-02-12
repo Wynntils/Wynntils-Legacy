40fa3a60f78ddca75f58d43dae9c15f017d4d113
PhoenixImpulse <85183285+PhoenixImpulse@users.noreply.github.com>
2022-02-12 11:46:24 +0000

    Update CommandServer.java

--- src/main/java/com/wynntils/modules/core/commands/CommandServer.java
+++ src/main/java/com/wynntils/modules/core/commands/CommandServer.java
@@ -93,6 +93,8 @@ public class CommandServer extends CommandBase implements IClientCommand {
                 .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
         
+        TextComponentString toSend = new TextComponentString("§bApproximate soul point times:");
+        
         for (String wynnServer : sortedServers.keySet()) {
             int uptimeMinutes = sortedServers.get(wynnServer);
             TextFormatting minuteColor;
