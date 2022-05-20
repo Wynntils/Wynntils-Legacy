8109d75cbeae98ff18ab2339ed1f39b609c0dd56
ByThePowerOfScience <16433721+ByThePowerOfScience@users.noreply.github.com>
2022-05-20 19:26:27 -0400

    Add check for 0 size to siblings
    Fix ArrayIndexOutOfBoundsException when there are zero siblings.

--- src/main/java/com/wynntils/modules/chat/managers/ChatManager.java
+++ src/main/java/com/wynntils/modules/chat/managers/ChatManager.java
@@ -906,7 +906,7 @@ public class ChatManager {
             for (int i = max; i < siblings.size(); i++) {
                 ITextComponent line = siblings.get(i).createCopy();
                 // Remove new line if present
-                if (i != siblings.size() - 1) {
+                if (siblings.size() != 0 && i != siblings.size() - 1) {
                     line.getSiblings().remove(line.getSiblings().size() - 1);
                 }
 
