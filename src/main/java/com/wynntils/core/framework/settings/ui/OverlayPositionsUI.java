dd4341fa2a69819fc9e23426c6e585b76d116712
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-06-02 21:00:38 -0700

    fix: other half of the fix in #526

--- src/main/java/com/wynntils/core/framework/settings/ui/OverlayPositionsUI.java
+++ src/main/java/com/wynntils/core/framework/settings/ui/OverlayPositionsUI.java
@@ -172,6 +172,9 @@ public class OverlayPositionsUI extends UI {
 
     @Override
     protected void mouseReleased(int mouseX, int mouseY, int state) {
+        if (state > 3) {
+            state = 3; // Set to UNKNOWN
+        }
         super.mouseReleased(mouseX, mouseY, state);
         for (OverlayButton button : registeredOverlaySettings) {
             if (button.isMouseButtonHeld()) {
@@ -186,6 +189,9 @@ public class OverlayPositionsUI extends UI {
 
     @Override
     protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
+        if (clickedMouseButton > 3) {
+            clickedMouseButton = 3; // Set to UNKNOWN
+        }
         super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
         for (OverlayButton button : registeredOverlaySettings) {
             if (button.isMouseButtonHeld()) {
