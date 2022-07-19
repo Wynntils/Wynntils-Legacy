9ab14d1adf9d28346b8bcd097d2be9ea52bd5c36
Ori <38721999+OriSpirit@users.noreply.github.com>
2022-07-19 15:15:03 +0800

    Update ChatOverlay.java
    Fix Issue #545 by implementing a simple null check

--- src/main/java/com/wynntils/modules/chat/overlays/ChatOverlay.java
+++ src/main/java/com/wynntils/modules/chat/overlays/ChatOverlay.java
@@ -478,6 +478,7 @@ public class ChatOverlay extends GuiNewChat {
 
         TabManager.getAvailableTabs().forEach(tab -> {
             if (tab == currentTab) return;
+            tab.getCurrentMessages().removeIf(Objects::isNull);
             tab.getCurrentMessages().removeIf(chatline -> chatline.getChatLineID() == id);
         });
 
