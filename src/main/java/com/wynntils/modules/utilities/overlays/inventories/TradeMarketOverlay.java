80d07c9f3531dd80f1108aed7f3860bac0be99bc
byBackfish <maik@bybackifsh.de>
2022-08-19 16:45:03 +0200

    Adds the ´Sell All` item to the inventory. Clicking that item will automatically select the amount.

--- src/main/java/com/wynntils/modules/utilities/overlays/inventories/TradeMarketOverlay.java
+++ src/main/java/com/wynntils/modules/utilities/overlays/inventories/TradeMarketOverlay.java
@@ -4,10 +4,11 @@ import com.mojang.realmsclient.gui.ChatFormatting;
 import com.wynntils.McIf;
 import com.wynntils.core.events.custom.GuiOverlapEvent;
 import com.wynntils.core.framework.interfaces.Listener;
-import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.nbt.*;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.regex.Matcher;
@@ -15,19 +16,36 @@ import java.util.regex.Pattern;
 
 public class TradeMarketOverlay implements Listener {
 
-    private final Pattern ITEM_NAME_PATTERN = Pattern.compile(".*Selling §f\\d+ (.*) for .* Each");
+    private final Pattern ITEM_NAME_PATTERN = Pattern.compile(".*Selling §f\\d+ (.*)§6 for .* Each");
+    private boolean shouldSend = false;
+    private int amountToSend = 0;
 
     @SubscribeEvent
-    public void onRender(GuiOverlapEvent.ChestOverlap.HandleMouseClick e) {
+    public void onGuiUpdate(GuiOverlapEvent.ChestOverlap.DrawScreen e) {
         InventoryBasic inventory = (InventoryBasic) e.getGui().getLowerInv();
+        if (!inventory.getName().startsWith("What would you like to sell?")) {
+            shouldSend = false;
+            return;
+        }
+        if (inventory.getStackInSlot(20) != ItemStack.EMPTY && !inventory.getStackInSlot(20).getDisplayName().isEmpty()) return;
 
+        ItemStack itemStack = new ItemStack(buildNBT());
+        itemStack.setTagCompound(buildNBT());
+
+        inventory.setInventorySlotContents(20, itemStack);
+        shouldSend = true;
+    }
+
+    @SubscribeEvent
+    public void onClick(GuiOverlapEvent.ChestOverlap.HandleMouseClick e) {
+        InventoryBasic inventory = (InventoryBasic) e.getGui().getLowerInv();
         if (!inventory.getName().contains("What would you like to sell?")) return;
-        if(e.getSlotId() != 11) return;
+        if (e.getSlotId() != 20) return;
 
         ItemStack itemStack = inventory.getStackInSlot(10);
 
         if (itemStack == ItemStack.EMPTY) return;
-        if (itemStack.getDisplayName().contains("Click an Item to Sell")) return;
+        if (itemStack.getDisplayName().contains("Click an Item to sell")) return;
 
         Matcher matcher = ITEM_NAME_PATTERN.matcher(itemStack.getDisplayName());
 
@@ -37,12 +55,50 @@ public class TradeMarketOverlay implements Listener {
         int amount = 0;
 
         for (ItemStack item : McIf.player().inventory.mainInventory) {
-            if (ChatFormatting.stripFormatting(item.getDisplayName()).equals(ChatFormatting.stripFormatting(itemName))) {
+            if (item.getDisplayName().toLowerCase().trim().equals(itemName.toLowerCase().trim())) {
                 int itemAmount = item.getCount();
                 amount += itemAmount;
             }
         }
 
-        Minecraft.getMinecraft().player.sendMessage(new TextComponentString(ChatFormatting.DARK_GRAY + "You have " + ChatFormatting.GOLD + ChatFormatting.BOLD + amount + " " + itemName + ChatFormatting.DARK_GRAY + " in your inventory"));
+        int slotId = 11;
+        shouldSend = true;
+        amountToSend = amount;
+        e.getGui().handleMouseClick(e.getGui().inventorySlots.getSlot(slotId), slotId, 2, ClickType.PICKUP);
+    }
+
+    @SubscribeEvent
+    public void onChatMessage(ClientChatReceivedEvent e) {
+        if (!shouldSend) return;
+
+        String message = ChatFormatting.stripFormatting(e.getMessage().getFormattedText());
+        if (!message.trim().startsWith("Type the amount you wish to sell or type 'cancel' to cancel:")) return;
+
+        McIf.player().sendChatMessage(String.valueOf(amountToSend));
+        shouldSend = false;
+    }
+
+    public NBTTagCompound buildNBT() {
+        NBTTagCompound nbt = new NBTTagCompound();
+        NBTTagCompound tag = new NBTTagCompound();
+
+        nbt.setString("id", "minecraft:emerald");
+        nbt.setInteger("Count", 1);
+
+        NBTTagCompound display = new NBTTagCompound();
+        display.setString("Name", "§aSell All");
+
+        NBTTagList lore = new NBTTagList();
+        lore.appendTag(new NBTTagString("§7Click to sell all items in your inventory"));
+        display.setTag("Lore", lore);
+
+        tag.setFloat("Unbreakable", 1);
+        tag.setInteger("HideFlags", 6);
+
+        nbt.setTag("display", display);
+        nbt.setTag("tag", tag);
+        nbt.setFloat("Damage", 0);
+
+        return nbt;
     }
 }
