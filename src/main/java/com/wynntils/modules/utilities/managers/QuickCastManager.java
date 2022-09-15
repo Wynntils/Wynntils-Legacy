995e5cc0c4fc5a0bec2ca54c67a8e2d5e399f66f
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-09-14 18:00:06 -0700

    feat: copy Artemis spell cast implementation

--- src/main/java/com/wynntils/modules/utilities/managers/QuickCastManager.java
+++ src/main/java/com/wynntils/modules/utilities/managers/QuickCastManager.java
@@ -5,33 +5,45 @@
 package com.wynntils.modules.utilities.managers;
 
 import com.wynntils.McIf;
+import com.wynntils.Reference;
+import com.wynntils.core.events.custom.PacketEvent;
+import com.wynntils.core.events.custom.WynnClassChangeEvent;
 import com.wynntils.core.framework.enums.ClassType;
 import com.wynntils.core.framework.instances.PlayerInfo;
 import com.wynntils.core.framework.instances.data.CharacterData;
+import com.wynntils.core.framework.interfaces.Listener;
 import com.wynntils.core.utils.ItemUtils;
-import com.wynntils.core.utils.helpers.Delay;
+import com.wynntils.core.utils.StringUtils;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.CPacketAnimation;
+import net.minecraft.network.play.client.CPacketHeldItemChange;
 import net.minecraft.network.play.client.CPacketPlayerDigging;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItem;
+import net.minecraft.network.play.server.SPacketChat;
+import net.minecraft.network.play.server.SPacketTitle;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.wynntils.core.framework.instances.data.SpellData.SPELL_LEFT;
 import static com.wynntils.core.framework.instances.data.SpellData.SPELL_RIGHT;
 
-public class QuickCastManager {
+public class QuickCastManager implements Listener {
 
     private static final CPacketAnimation leftClick = new CPacketAnimation(EnumHand.MAIN_HAND);
     private static final CPacketPlayerTryUseItem rightClick = new CPacketPlayerTryUseItem(EnumHand.MAIN_HAND);
-    private static final CPacketPlayerDigging releaseClick = new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN);
+    // private static final CPacketPlayerDigging releaseClick = new CPacketPlayerDigging(CPacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN);
 
     private enum CastCheckStatus {
         CHAR_LEVEL_REQ_NOT_MET,
@@ -43,20 +55,26 @@ public class QuickCastManager {
     }
 
     private static final int[] spellUnlock = { 1, 6, 10, 10 };
-    private static long earliestCastable = System.currentTimeMillis();
-    private static final int delayTicks = 3; // 3 ticks = 150ms (6.666 cps), 2 ticks = 100ms (10 cps)
+    private static final Pattern SPELL_PATTERN =
+            StringUtils.compileCCRegex("§([LR]|Right|Left)§-§([LR?]|Right|Left)§-§([LR?]|Right|Left)§");
     private static final Pattern CLASS_REQ_OK_PATTERN = Pattern.compile("§a✔§7 Class Req:.+");
     private static final Pattern COMBAT_LVL_REQ_OK_PATTERN = Pattern.compile("§a✔§7 Combat Lv. Min:.+");
     private static final Pattern SKILL_POINT_MIN_NOT_REACHED_PATTERN = Pattern.compile("§c✖§7 (.+) Min: \\d+");
+
+    private static final Queue<Packet<?>> spellPacketQueue = new LinkedList<>();
+    private static List<Boolean> spellInProgress = new ArrayList<>(3);
+    private static int lastSelectedSlot = 0;
+    private static int spellResetCountdown = 0;
+    private static int packetQueueCountdown = 0;
 
     /**
      * Queues the spell to be sent to the server.
      * true in boolean fields represents right click.
      */
-    private static void queueSpell(boolean second, boolean third) {
-        if (System.currentTimeMillis() < earliestCastable) {
+    private static void queueSpell(boolean first, boolean second, boolean third) {
+        if (!spellPacketQueue.isEmpty()) {
             McIf.player().sendMessage(new TextComponentString(
-                    TextFormatting.GRAY + "Cannot start casting a spell while another spell cast is in progress."
+                    TextFormatting.GRAY + "Cannot cast a spell while another spell cast is in progress."
             ));
             return;
         }
@@ -64,24 +82,105 @@ public class QuickCastManager {
         NetHandlerPlayClient connection = McIf.mc().getConnection();
         if (connection == null) return;
 
-        boolean first = SPELL_RIGHT;
-        if (PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER) {
-            // Archer spells are inverted
-            first = !first;
-            second = !second;
-            third = !third;
-        }
-        // Variables used in the lambda expressions below should be final
-        final boolean finalSecond = second;
-        final boolean finalThird = third;
+        boolean isArcher = PlayerInfo.get(CharacterData.class).getCurrentClass() == ClassType.ARCHER;
 
-        connection.sendPacket(first ? rightClick : leftClick);
-        new Delay(() -> connection.sendPacket(finalSecond ? rightClick : leftClick), delayTicks);
-        new Delay(() -> connection.sendPacket(finalThird ? rightClick : leftClick), delayTicks * 2);
-        // earliestCastable = (current time) + (delay in ticks * 3) * (50ms per tick) + (10ms buffer)
-        // * 3 so there is still a delay between separate casts
-        // + 10L as another buffer to prevent issues when spamming
-        earliestCastable = System.currentTimeMillis() + delayTicks * 3L * 50L + 10L;
+        List<Boolean> spell = new ArrayList<>(3);
+        spell.add(isArcher != first); // Inverts booleans (r -> l, l -> r) if archer
+        spell.add(isArcher != second);
+        spell.add(isArcher != third);
+
+        System.out.println(spellInProgress);
+        for (int i = 0; i < spellInProgress.size(); i++) {
+            if (spellInProgress.get(i) != spell.get(i)) {
+                McIf.player().sendMessage(new TextComponentString(
+                        TextFormatting.GRAY + "Finish your incompatible spell-cast first."
+                ));
+                return;
+            }
+        }
+
+        lastSelectedSlot = McIf.player().inventory.currentItem;
+        List<Boolean> remainderToCast = spell.subList(spellInProgress.size(), spell.size());
+        remainderToCast.stream().map(QuickCastManager::getPacket).forEach(spellPacketQueue::add);
+    }
+
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent e) {
+        if (!Reference.onWorld) return;
+
+        // Clear spell after the 40 tick timeout period
+        if (spellResetCountdown > 0 && --spellResetCountdown <= 0) {
+            spellInProgress.clear();
+        }
+
+        if (spellPacketQueue.isEmpty()) return; // Nothing to cast, return
+        if (--packetQueueCountdown > 0) return; // Don't cast on this tick, return
+
+        if (McIf.mc().currentScreen != null) return; // Don't cast if a GUI is open
+
+        int currentSelectedSlot = McIf.player().inventory.currentItem;
+        boolean slotChanged = currentSelectedSlot != lastSelectedSlot;
+
+        NetHandlerPlayClient connection = McIf.mc().getConnection();
+        if (connection == null) return;
+
+        if (slotChanged) connection.sendPacket(new CPacketHeldItemChange(lastSelectedSlot));
+        connection.sendPacket(spellPacketQueue.remove());
+        if (slotChanged) connection.sendPacket(new CPacketHeldItemChange(currentSelectedSlot));
+
+        // Number of ticks to delay between spell cast button inputs
+        if (!spellPacketQueue.isEmpty()) packetQueueCountdown = 4;
+    }
+
+    @SubscribeEvent
+    public void onClassChange(WynnClassChangeEvent e) {
+        spellPacketQueue.clear();
+        spellInProgress.clear();
+    }
+
+    @SubscribeEvent
+    public void onSubtitleUpdate(PacketEvent<SPacketTitle> e) {
+        if (!Reference.onWorld || e.getPacket().getType() != SPacketTitle.Type.SUBTITLE) return;
+        // Subtitle for low level players
+        tryUpdateSpell(e.getPacket().getMessage().getUnformattedText());
+    }
+
+    @SubscribeEvent
+    public void onActionbarMessageUpdate(PacketEvent<SPacketChat> e) {
+        if (!Reference.onWorld || e.getPacket().getType() != ChatType.GAME_INFO) return;
+
+        tryUpdateSpell(e.getPacket().getChatComponent().getUnformattedText());
+    }
+
+    private static void tryUpdateSpell(String text) {
+        List<Boolean> spell = getSpellFromString(text);
+        if (spell == null) return;
+        if (spellInProgress.equals(spell)) return;
+        if (spell.size() == 3) {
+            spellInProgress.clear();
+            spellResetCountdown = 0;
+        } else {
+            spellInProgress = spell;
+            spellResetCountdown = 40;
+        }
+
+    }
+
+    private static List<Boolean> getSpellFromString(String s) {
+        Matcher spellMatcher = SPELL_PATTERN.matcher(s);
+        if (!spellMatcher.find()) return null;
+
+        int size = 1;
+        for (; size < 3; ++size) {
+            if (spellMatcher.group(size + 1).equals("?")) break;
+        }
+
+        List<Boolean> spell = new ArrayList<>(3);
+        for (int i = 0; i < size; ++i) {
+            spell.add(i, spellMatcher.group(i + 1).charAt(0) == 'R');
+        }
+
+        return spell;
     }
 
     private static CastCheckStatus checkSpellCastRequest(int spellNumber) {
@@ -133,23 +232,27 @@ public class QuickCastManager {
         return CastCheckStatus.OK;
     }
 
+    private static Packet<?> getPacket(boolean direction) {
+        return direction ? rightClick : leftClick;
+    }
+
     public static void castFirstSpell() {
         if (checkSpellCastRequest(1) != CastCheckStatus.OK) return;
-        queueSpell(SPELL_LEFT, SPELL_RIGHT);
+        queueSpell(SPELL_RIGHT, SPELL_LEFT, SPELL_RIGHT);
     }
 
     public static void castSecondSpell() {
         if (checkSpellCastRequest(2) != CastCheckStatus.OK) return;
-        queueSpell(SPELL_RIGHT, SPELL_RIGHT);
+        queueSpell(SPELL_RIGHT, SPELL_RIGHT, SPELL_RIGHT);
     }
 
     public static void castThirdSpell() {
         if (checkSpellCastRequest(3) != CastCheckStatus.OK) return;
-        queueSpell(SPELL_LEFT, SPELL_LEFT);
+        queueSpell(SPELL_RIGHT, SPELL_LEFT, SPELL_LEFT);
     }
 
     public static void castFourthSpell() {
         if (checkSpellCastRequest(4) != CastCheckStatus.OK) return;
-        queueSpell(SPELL_RIGHT, SPELL_LEFT);
+        queueSpell(SPELL_RIGHT, SPELL_RIGHT, SPELL_LEFT);
     }
 }
