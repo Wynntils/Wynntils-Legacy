982684480911d127813a9f1184c42565ce8b8498
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-09-29 17:11:20 -0700

    feat: initial impl of location-updating totems w/o cast reliance

--- src/main/java/com/wynntils/modules/core/instances/ShamanTotemTracker.java
+++ src/main/java/com/wynntils/modules/core/instances/ShamanTotemTracker.java
@@ -18,12 +18,10 @@ import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.network.play.client.CPacketHeldItemChange;
 import net.minecraft.network.play.server.SPacketDestroyEntities;
 import net.minecraft.network.play.server.SPacketEntityMetadata;
-import net.minecraft.network.play.server.SPacketSpawnObject;
 import net.minecraftforge.fml.common.eventhandler.Event;
+import org.apache.logging.log4j.Level;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -38,22 +36,12 @@ public class ShamanTotemTracker {
     private ShamanTotem totem2 = null;
 
     private long totemCastTimestamp = 0;
-    private long totemCreatedTimestamp = Long.MAX_VALUE;
-
-    private int potentialId = -1;
-    private double potentialX, potentialY, potentialZ;
-
     private int summonWeaponSlot = -1; // Weapon used to summon totem(s)
 
     private int bufferedId = -1;
     private double bufferedX = -1;
     private double bufferedY = -1;
     private double bufferedZ = -1;
-
-    private static boolean isClose(double a, double b) {
-        double diff = Math.abs(a - b);
-        return (diff < 3);
-    }
 
     private void postEvent(Event event) {
         McIf.mc().addScheduledTask(() -> FrameworkManager.getEventBus().post(event));
@@ -68,25 +56,6 @@ public class ShamanTotemTracker {
         }
 
         return null;
-    }
-
-    private void checkTotemSummoned() {
-        // Check if we have both creation and spell cast at roughly the same time
-        if (Math.abs(totemCreatedTimestamp - totemCastTimestamp) < 500) {
-            ShamanTotem newTotem = new ShamanTotem(potentialId, -1, -1, TotemState.SUMMONED, new Location(potentialX, potentialY, potentialZ));
-            if (totem1 == null) {
-                totem1 = newTotem;
-                postEvent(new SpellEvent.TotemSummoned(1));
-            } else {
-                totem2 = newTotem;
-                postEvent(new SpellEvent.TotemSummoned(2));
-            }
-        } else if (Math.abs(totemCreatedTimestamp - totemCastTimestamp) < 3500 && totem1 != null) { // This is our flexible totem cast check
-            // Only triggers if we have a totem1, and the totem was created within 3.5 seconds of the cast
-            // This is because Wynn's server does not trigger another spell cast packet if you cast two totems quickly in succession
-            totem2 = new ShamanTotem(potentialId, -1, -1, TotemState.SUMMONED, new Location(potentialX, potentialY, potentialZ));
-            postEvent(new SpellEvent.TotemSummoned(2));
-        }
     }
 
     /**
@@ -106,51 +75,12 @@ public class ShamanTotemTracker {
     }
 
     /**
-     * Removes all totems from the list of totems.
+     * Resets both totem variables.
      */
     private void removeAllTotems() {
         summonWeaponSlot = -1;
         removeTotem(1);
         removeTotem(2);
-    }
-
-    public void onTotemSpawn(PacketEvent<SPacketSpawnObject> e) {
-        if (!Reference.onWorld || e.getPacket().getType() != 78) return;
-
-        double packetX = e.getPacket().getX();
-        double packetY = e.getPacket().getY();
-        double packetZ = e.getPacket().getZ();
-
-        bufferedId = e.getPacket().getEntityID();
-        bufferedX = packetX;
-        bufferedY = packetY;
-        bufferedZ = packetZ;
-
-        if (totem1 != null && e.getPacket().getEntityID() == totem1.getId() && totem1.getState() == TotemState.SUMMONED) {
-            // Totems respawn with the same entityID when landing
-            // Update with more precise coordinates
-            totem1.setLocation(new Location(packetX, packetY, packetZ));
-            totem1.setState(TotemState.ACTIVE);
-        } else if (totem2 != null && e.getPacket().getEntityID() == totem2.getId() && totem2.getState() == TotemState.SUMMONED) {
-            // Totems respawn with the same entityID when landing
-            // Update with more precise coordinates
-            totem2.setLocation(new Location(packetX, packetY, packetZ));
-            totem2.setState(TotemState.ACTIVE);
-        }
-
-        // Is the new entity created close to us? If so, it could be a new totem
-        // This check works because the totem flies out from the player when the totem spell is cast
-        if (isClose(packetX, McIf.player().posX) &&
-                isClose(packetY, McIf.player().posY) &&
-                isClose(packetZ, McIf.player().posZ)) {
-            potentialId = e.getPacket().getEntityID();
-            potentialX = packetX;
-            potentialY = packetY;
-            potentialZ = packetZ;
-            totemCreatedTimestamp = System.currentTimeMillis();
-        }
-        checkTotemSummoned(); // Go check if it actually is a totem
-
     }
 
     public void onTotemSpellCast(SpellEvent.Cast e) {
@@ -161,8 +91,6 @@ public class ShamanTotemTracker {
     }
 
     public void onTotemRename(PacketEvent<SPacketEntityMetadata> e) {
-        System.out.println(totem1);
-        System.out.println(totem2);
         if (!Reference.onWorld) return;
 
         String name = Utils.getNameFromMetadata(e.getPacket().getDataManagerEntries());
@@ -173,58 +101,42 @@ public class ShamanTotemTracker {
         if (!(entity instanceof EntityArmorStand)) return;
 
         /*
-        If the totem has this timer bound, process matcher
-        OR
-        If the totem does not have a timer bound and this timer is not bound, process matcher
-        ELSE
-        Skip this totem
-        */
-        if (totem1 != null && ((totem1.getTimerId() == entityId) || (!timerIdAlreadyBound(entityId) && totem1.getTimerId() == -1))) {
-            Matcher m = SHAMAN_TOTEM_TIMER.matcher(name);
-            if (m.matches()) {
-                // This matcher matches when the totem is on the ground
-                // This doesn't necessarily mean that the TotemState is ACTIVE, it could be the first time the timer has ticked after the totem landed
-                // We also check if the timerId is -1 or the same as the timer entity's ID to make sure we don't confuse the two timers
-                totem1.setState(TotemState.ACTIVE);
-                totem1.setLocation(new Location(entity.posX, entity.posY - 2.8, entity.posZ));
-                totem1.setTimerId(entityId);
+        Logic flow for the following bits:
+        - First, the given entity is checked to see if it is a totem timer
+        - If the given timerId (int entityId) is not already a totem, assign it to the lowest # totem slot
+          - Additionally, assign location, state, and time
+        - If the given timerId is already a totem, update the time and location instead
+          - Location is updated because there are now totems that can move
+         */
+        Matcher m = SHAMAN_TOTEM_TIMER.matcher(name);
+        if (!m.find()) return;
 
-                int parsedTime = Integer.parseInt(m.group(1));
-                if (totem1.getTime() == -1) {
-                    totem1.setTime(parsedTime);
-                    postEvent(new SpellEvent.TotemActivated(1, totem1.getTime(), totem1.getLocation()));
-                } else if (parsedTime != totem1.getTime()) {
-                    if (parsedTime > totem1.getTime()) {
-                        // Timer restarted using uproot
-                        postEvent(new SpellEvent.TotemRenewed(1, parsedTime, totem1.getLocation()));
-                    }
-                    totem1.setTime(parsedTime); // Desync, reset to in-game time
-                }
+        int parsedTime = Integer.parseInt(m.group(1));
+        Location parsedLocation = new Location(entity.posX, entity.posY, entity.posZ);
+
+        if (getBoundTotem(entityId) == null && Math.abs(totemCastTimestamp - System.currentTimeMillis()) < 15000) {
+            // Given timerId is not a totem, make a new totem (assuming regex matches and we are within 15s of casting)
+            ShamanTotem newTotem = new ShamanTotem(entityId, parsedTime, TotemState.ACTIVE, parsedLocation);
+            if (totem1 == null) {
+                totem1 = newTotem;
+                postEvent(new SpellEvent.TotemActivated(1, parsedTime, parsedLocation));
+            } else if (totem2 == null) {
+                totem2 = newTotem;
+                postEvent(new SpellEvent.TotemActivated(2, parsedTime, parsedLocation));
+            } else {
+                // No totem slots available?
+                Reference.LOGGER.log(Level.WARN, "Received a new totem " + entityId + ", but no totem slots are available");
             }
-        }
-
-        if (totem2 != null && ((totem2.getTimerId() == entityId) || (!timerIdAlreadyBound(entityId) && totem2.getTimerId() == -1))) {
-            Matcher m = SHAMAN_TOTEM_TIMER.matcher(name);
-            if (m.matches()) {
-                // This matcher matches when the totem is on the ground
-                // This doesn't necessarily mean that the TotemState is ACTIVE, it could be the first time the timer has ticked after the totem landed
-                // We also check if the timerId is -1 or the same as the timer entity's ID to make sure we don't confuse the two timers
-                totem2.setState(TotemState.ACTIVE);
-                totem2.setLocation(new Location(entity.posX, entity.posY - 2.8, entity.posZ));
-                totem2.setTimerId(entityId);
-
-                int parsedTime = Integer.parseInt(m.group(1));
-                if (totem2.getTime() == -1) {
-                    totem2.setTime(parsedTime);
-                    postEvent(new SpellEvent.TotemActivated(2, totem2.getTime(), totem2.getLocation()));
-                } else if (parsedTime != totem2.getTime()) {
-                    if (parsedTime > totem2.getTime()) {
-                        // Timer restarted using uproot
-                        postEvent(new SpellEvent.TotemRenewed(2, parsedTime, totem2.getLocation()));
-                    }
-                    totem2.setTime(parsedTime); // Desync, reset to in-game time
-                }
-            }
+        } else if (getBoundTotem(entityId) == totem1) {
+            // Update totem 1
+            totem1.setTime(parsedTime);
+            totem1.setLocation(parsedLocation);
+            postEvent(new SpellEvent.TotemActivated(1, parsedTime, parsedLocation));
+        } else if (getBoundTotem(entityId) == totem2) {
+            // Update totem 2
+            totem2.setTime(parsedTime);
+            totem2.setLocation(parsedLocation);
+            postEvent(new SpellEvent.TotemActivated(2, parsedTime, parsedLocation));
         }
     }
 
@@ -233,10 +145,10 @@ public class ShamanTotemTracker {
 
         List<Integer> destroyedEntities = Arrays.stream(e.getPacket().getEntityIDs()).boxed().collect(Collectors.toList());
 
-        if (totem1 != null && (destroyedEntities.contains(totem1.getId()) || destroyedEntities.contains(totem1.getTimerId()))) {
+        if (totem1 != null && destroyedEntities.contains(totem1.getTimerId())) {
             removeTotem(1);
         }
-        if (totem2 != null && (destroyedEntities.contains(totem2.getId()) || destroyedEntities.contains(totem2.getTimerId()))) {
+        if (totem2 != null && destroyedEntities.contains(totem2.getTimerId())) {
             removeTotem(2);
         }
     }
@@ -254,32 +166,27 @@ public class ShamanTotemTracker {
     }
 
     /**
-     * Checks if the given entity ID is already bound as a totem's timer.
-     * @param id The entity ID to check
-     * @return true if the timer ID is bound, false otherwise
+     * Gets the totem bound to the given timerId.
+     * @param timerId The timerId that is checked against the totems
+     * @return The totem bound to the given timerId, or null if no totem is bound
      */
-    private boolean timerIdAlreadyBound(int id) {
-        if (totem1 == null || totem2 == null) return false;
-        return totem1.getTimerId() == id || totem2.getTimerId() == id;
+    private ShamanTotem getBoundTotem(int timerId) {
+        if (totem1 != null && totem1.getTimerId() == timerId) return totem1;
+        if (totem2 != null && totem2.getTimerId() == timerId) return totem2;
+        return null;
     }
 
     public static class ShamanTotem {
-        private final int id;
         private int timerId;
         private int time;
         private TotemState state;
         private Location location;
 
-        public ShamanTotem(int id, int timerId, int time, TotemState state, Location location) {
-            this.id = id;
+        public ShamanTotem(int timerId, int time, TotemState state, Location location) {
             this.timerId = timerId;
             this.time = time;
             this.state = state;
             this.location = location;
-        }
-
-        public int getId() {
-            return id;
         }
 
         public int getTimerId() {
